ed64ba85cf1c40340c7d4f0bcefe29c4e3ac68c6
pmahendro <pmahendro@bloomingtech-tp>
2022-12-23 16:15:59 +0700

    Add crud master currency

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('memo', './assets/styles/memo.css')
+    .addStyleEntry('report', './assets/styles/report.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
