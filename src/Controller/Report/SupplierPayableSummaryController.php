71d60da489ba89c8771e087b6b45fcf8fcc52db0
pmahendro <pmahendro@bloomingtech-tp>
2023-01-02 22:32:21 +0700

    Modify purchase invoice controller + add controller report payable

--- src/Controller/Report/SupplierPayableSummaryController.php
+++ src/Controller/Report/SupplierPayableSummaryController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/report/purchase_invoice_header')]
-class PurchaseInvoiceHeaderController extends AbstractController
+#[Route('/report/supplier_payable_summary')]
+class SupplierPayableSummaryController extends AbstractController
 {
-    #[Route('/_list', name: 'app_report_purchase_invoice_header__list', methods: ['GET'])]
+    #[Route('/_list', name: 'app_report_supplier_payable_summary__list', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
     public function _list(Request $request, PurchaseInvoiceHeaderRepository $purchaseInvoiceHeaderRepository): Response
     {
@@ -22,20 +22,22 @@ class PurchaseInvoiceHeaderController extends AbstractController
         $form = $this->createForm(PurchaseInvoiceHeaderGridType::class, $criteria, ['method' => 'GET']);
         $form->handleRequest($request);
 
-        list($count, $purchaseInvoiceHeaders) = $purchaseInvoiceHeaderRepository->fetchData($criteria);
+        list($count, $purchaseInvoiceHeaders) = $purchaseInvoiceHeaderRepository->fetchData($criteria, function($qb, $alias) {
+            $qb->andWhere("{$alias}.remainingPayment > 0");
+        });
 
-        return $this->renderForm("report/purchase_invoice_header/_list.html.twig", [
+        return $this->renderForm("report/supplier_payable_summary/_list.html.twig", [
             'form' => $form,
             'count' => $count,
             'purchaseInvoiceHeaders' => $purchaseInvoiceHeaders,
         ]);
     }
 
-    #[Route('/', name: 'app_report_purchase_invoice_header_index', methods: ['GET'])]
+    #[Route('/', name: 'app_report_supplier_payable_summary_index', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        return $this->render("report/purchase_invoice_header/index.html.twig");
+        return $this->render("report/supplier_payable_summary/index.html.twig");
     }
 
 //    #[Route('/export', name: 'app_report_purchase_invoice_header_export', methods: ['GET'])]
