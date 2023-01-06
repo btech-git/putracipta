2fd2e1a8ae44f644eeb95a21ce0fb7bbff5f9ac9
pmahendro <pmahendro@bloomingtech-tp>
2023-01-06 16:48:36 +0700

    Modify exception checking in entity add and remove common library

--- src/Common/Doctrine/Repository/EntityRemove.php
+++ src/Common/Doctrine/Repository/EntityRemove.php
@@ -6,7 +6,7 @@ trait EntityRemove
 {
     public function remove($entity, bool $flush = false): void
     {
-        if (get_class($entity) !== $this->getEntityName()) {
+        if (!str_contains(get_class($entity), $this->getEntityName())) {
             throw new \Exception('An invalid entity was passed as an argument');
         }
 
