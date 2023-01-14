30367fc8e0966428daf3aa720db2b6fdc0f11ac0
pmahendro <pmahendro@bloomingtech-tp>
2023-01-14 16:07:37 +0700

    Modify shared filter and sort on grid

--- src/Grid/Shared/SaleOrderHeaderGridType.php
+++ src/Grid/Shared/SaleOrderHeaderGridType.php
@@ -22,49 +22,37 @@ class SaleOrderHeaderGridType extends AbstractType
     {
         $builder
             ->add('filter', FilterType::class, [
-                'field_names' => ['discountValueType', 'discountValue', 'taxMode', 'taxPercentage', 'taxNominal', 'shippingFee', 'subTotal', 'subTotalAfterTaxInclusion', 'grandTotal', 'isCanceled', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'createdTransactionDateTime', 'modifiedTransactionDateTime', 'approvedTransactionDateTime', 'transactionDate', 'note'],
+                'field_names' => ['codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate', 'grandTotal', 'note', 'transactionStatus', 'isCanceled'],
                 'field_operators_list' => [
-                    'discountValueType' => [FilterContain::class, FilterNotContain::class],
-                    'discountValue' => [FilterEqual::class, FilterNotEqual::class],
-                    'taxMode' => [FilterContain::class, FilterNotContain::class],
-                    'taxPercentage' => [FilterEqual::class, FilterNotEqual::class],
-                    'taxNominal' => [FilterEqual::class, FilterNotEqual::class],
-                    'shippingFee' => [FilterEqual::class, FilterNotEqual::class],
-                    'subTotal' => [FilterEqual::class, FilterNotEqual::class],
-                    'subTotalAfterTaxInclusion' => [FilterEqual::class, FilterNotEqual::class],
                     'grandTotal' => [FilterEqual::class, FilterNotEqual::class],
                     'isCanceled' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberOrdinal' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberMonth' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberYear' => [FilterEqual::class, FilterNotEqual::class],
-                    'createdTransactionDateTime' => [FilterEqual::class, FilterNotEqual::class],
-                    'modifiedTransactionDateTime' => [FilterEqual::class, FilterNotEqual::class],
-                    'approvedTransactionDateTime' => [FilterEqual::class, FilterNotEqual::class],
                     'transactionDate' => [FilterEqual::class, FilterNotEqual::class],
                     'note' => [FilterContain::class, FilterNotContain::class],
+                    'transactionStatus' => [FilterEqual::class, FilterNotEqual::class],
+                ],
+                'field_value_type_list' => [
+                    'codeNumberOrdinal' => IntegerType::class,
+                    'codeNumberMonth' => ChoiceType::class,
+                    'codeNumberYear' => IntegerType::class,
+                ],
+                'field_value_options_list' => [
+                    'codeNumberMonth' => ['choices' => array_flip(TransactionHeader::MONTH_ROMAN_NUMERALS)],
                 ],
             ])
             ->add('sort', SortType::class, [
-                'field_names' => ['discountValueType', 'discountValue', 'taxMode', 'taxPercentage', 'taxNominal', 'shippingFee', 'subTotal', 'subTotalAfterTaxInclusion', 'grandTotal', 'isCanceled', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'createdTransactionDateTime', 'modifiedTransactionDateTime', 'approvedTransactionDateTime', 'transactionDate', 'note'],
+                'field_names' => ['codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate', 'grandTotal', 'note', 'transactionStatus', 'isCanceled'],
                 'field_operators_list' => [
-                    'discountValueType' => [SortAscending::class, SortDescending::class],
-                    'discountValue' => [SortAscending::class, SortDescending::class],
-                    'taxMode' => [SortAscending::class, SortDescending::class],
-                    'taxPercentage' => [SortAscending::class, SortDescending::class],
-                    'taxNominal' => [SortAscending::class, SortDescending::class],
-                    'shippingFee' => [SortAscending::class, SortDescending::class],
-                    'subTotal' => [SortAscending::class, SortDescending::class],
-                    'subTotalAfterTaxInclusion' => [SortAscending::class, SortDescending::class],
                     'grandTotal' => [SortAscending::class, SortDescending::class],
                     'isCanceled' => [SortAscending::class, SortDescending::class],
                     'codeNumberOrdinal' => [SortAscending::class, SortDescending::class],
                     'codeNumberMonth' => [SortAscending::class, SortDescending::class],
                     'codeNumberYear' => [SortAscending::class, SortDescending::class],
-                    'createdTransactionDateTime' => [SortAscending::class, SortDescending::class],
-                    'modifiedTransactionDateTime' => [SortAscending::class, SortDescending::class],
-                    'approvedTransactionDateTime' => [SortAscending::class, SortDescending::class],
                     'transactionDate' => [SortAscending::class, SortDescending::class],
                     'note' => [SortAscending::class, SortDescending::class],
+                    'transactionStatus' => [SortAscending::class, SortDescending::class],
                 ],
             ])
             ->add('pagination', PaginationType::class, ['size_choices' => [10, 20, 50, 100]])
