7f6ef975265d205d3801a9e42ece6e714fbb9955
pmahendro <pmahendro@bloomingtech-tp>
2023-01-14 13:31:51 +0700

    Modify repo

--- src/Repository/Transaction/PurchaseRequestDetailRepository.php
+++ src/Repository/Transaction/PurchaseRequestDetailRepository.php
@@ -9,14 +9,6 @@ use App\Entity\Transaction\PurchaseRequestDetail;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<PurchaseRequestDetail>
- *
- * @method PurchaseRequestDetail|null find($id, $lockMode = null, $lockVersion = null)
- * @method PurchaseRequestDetail|null findOneBy(array $criteria, array $orderBy = null)
- * @method PurchaseRequestDetail[]    findAll()
- * @method PurchaseRequestDetail[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 class PurchaseRequestDetailRepository extends ServiceEntityRepository
 {
     use EntityDataFetch, EntityAdd, EntityRemove;
