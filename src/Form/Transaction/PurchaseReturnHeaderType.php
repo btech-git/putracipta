288ce1605fb673e0b435847aa86894c521186e04
pmahendro <pmahendro@bloomingtech-tp>
2023-02-05 18:06:36 +0700

    Modify transaction form type

--- src/Form/Transaction/PurchaseReturnHeaderType.php
+++ src/Form/Transaction/PurchaseReturnHeaderType.php
@@ -7,6 +7,7 @@ use App\Entity\Transaction\PurchaseReturnDetail;
 use App\Entity\Transaction\PurchaseReturnHeader;
 use App\Entity\Transaction\ReceiveHeader;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,6 +19,11 @@ class PurchaseReturnHeaderType extends AbstractType
         $builder
             ->add('transactionDate', null, ['widget' => 'single_text'])
             ->add('note')
+            ->add('taxMode', ChoiceType::class, ['choices' => [
+                'Non PPn' => PurchaseReturnHeader::TAX_MODE_NON_TAX,
+                'PPn' => PurchaseReturnHeader::TAX_MODE_TAX_EXCLUSION,
+//                'Include PPn' => SaleReturnHeader::TAX_MODE_TAX_INCLUSION,
+            ]])
             ->add('receiveHeader', EntityHiddenType::class, ['class' => ReceiveHeader::class])
             ->add('purchaseReturnDetails', CollectionType::class, [
                 'entry_type' => PurchaseReturnDetailType::class,
