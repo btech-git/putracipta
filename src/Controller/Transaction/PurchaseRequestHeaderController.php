d9c0cadd1a000307ea8322874308f012dfea503f
darkstar <darkstar@lenovo>
2023-02-14 08:13:25 +0700

    Add datepicker and dropdown filters to purchase request header

--- src/Controller/Transaction/PurchaseRequestHeaderController.php
+++ src/Controller/Transaction/PurchaseRequestHeaderController.php
@@ -26,9 +26,8 @@ class PurchaseRequestHeaderController extends AbstractController
         $form->handleRequest($request);
 
         list($count, $purchaseRequestHeaders) = $purchaseRequestHeaderRepository->fetchData($criteria, function($qb, $alias, $add) use ($request) {
-            if (isset($request->query->get('purchase_request_header_grid')['filter']['warehouse:name']) && isset($request->query->get('purchase_request_header_grid')['sort']['warehouse:name'])) {
+            if (isset($request->query->get('purchase_request_header_grid')['sort']['warehouse:name'])) {
                 $qb->innerJoin("{$alias}.warehouse", 'w');
-                $add['filter']($qb, 'w', 'name', $request->query->get('purchase_request_header_grid')['filter']['warehouse:name']);
                 $add['sort']($qb, 'w', 'name', $request->query->get('purchase_request_header_grid')['sort']['warehouse:name']);
             }
         });
