9befd73250b5d07772ef95d1b71614ba2d08cfe6
pmahendro <pmahendro@bloomingtech-tp>
2023-03-02 17:36:31 +0700

    Modify work order grid type

--- src/Grid/Production/WorkOrderHeaderGridType.php
+++ src/Grid/Production/WorkOrderHeaderGridType.php
@@ -19,7 +19,7 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class WorkOrderGridType extends AbstractType
+class WorkOrderHeaderGridType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
