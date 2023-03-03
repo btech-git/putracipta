4df3fb5d140687e6469b4e9d8ebe721f1481de61
pmahendro <pmahendro@bloomingtech-tp>
2023-03-03 13:52:27 +0700

    Modify head dashboard to homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Common\Data\Criteria\DataCriteria;
+use App\Entity\Transaction\PurchaseOrderHeader;
+use App\Entity\Transaction\PurchaseOrderPaperHeader;
+use App\Entity\Transaction\PurchaseInvoiceHeader;
+use App\Entity\Transaction\SaleOrderHeader;
+use App\Entity\Transaction\SaleInvoiceHeader;
 use App\Form\Admin\UserType;
 use App\Repository\Admin\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -17,6 +24,46 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class DefaultController extends AbstractController
 {
+    #[Route('/_dashboard', name: 'app__dashboard', methods: ['GET'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function _dashboard(EntityManagerInterface $entityManager): Response
+    {
+        $criteria = new DataCriteria();
+        $purchaseOrderHeaderRepository = $entityManager->getRepository(PurchaseOrderHeader::class);
+        $purchaseOrderHeaderCount = $purchaseOrderHeaderRepository->fetchCount($criteria, function($qb, $alias) {
+            $qb->andWhere("{$alias}.isCanceled = false");
+            $qb->andWhere("{$alias}.isRead = false");
+        });
+        $purchaseOrderPaperHeaderRepository = $entityManager->getRepository(PurchaseOrderPaperHeader::class);
+        $purchaseOrderPaperHeaderCount = $purchaseOrderPaperHeaderRepository->fetchCount($criteria, function($qb, $alias) {
+            $qb->andWhere("{$alias}.isCanceled = false");
+            $qb->andWhere("{$alias}.isRead = false");
+        });
+        $purchaseInvoiceHeaderRepository = $entityManager->getRepository(PurchaseInvoiceHeader::class);
+        $purchaseInvoiceHeaderCount = $purchaseInvoiceHeaderRepository->fetchCount($criteria, function($qb, $alias) {
+            $qb->andWhere("{$alias}.isCanceled = false");
+            $qb->andWhere("{$alias}.isRead = false");
+        });
+        $saleOrderHeaderRepository = $entityManager->getRepository(SaleOrderHeader::class);
+        $saleOrderHeaderCount = $saleOrderHeaderRepository->fetchCount($criteria, function($qb, $alias) {
+            $qb->andWhere("{$alias}.isCanceled = false");
+            $qb->andWhere("{$alias}.isRead = false");
+        });
+        $saleInvoiceHeaderRepository = $entityManager->getRepository(SaleInvoiceHeader::class);
+        $saleInvoiceHeaderCount = $saleInvoiceHeaderRepository->fetchCount($criteria, function($qb, $alias) {
+            $qb->andWhere("{$alias}.isCanceled = false");
+            $qb->andWhere("{$alias}.isRead = false");
+        });
+
+        return $this->render('default/_dashboard.html.twig', [
+            'purchaseOrderHeaderCount' => $purchaseOrderHeaderCount,
+            'purchaseOrderPaperHeaderCount' => $purchaseOrderPaperHeaderCount,
+            'purchaseInvoiceHeaderCount' => $purchaseInvoiceHeaderCount,
+            'saleOrderHeaderCount' => $saleOrderHeaderCount,
+            'saleInvoiceHeaderCount' => $saleInvoiceHeaderCount,
+        ]);
+    }
+
     #[Route('/', name: 'app_index', methods: ['GET'])]
     public function index(): Response
     {
