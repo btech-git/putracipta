c1a1410ba3682ed5da85ba08a95b6b2ca5e94b68
pmahendro <pmahendro@bloomingtech-tp>
2023-03-08 09:38:41 +0700

    Modify master shared grid type

--- src/Grid/Shared/AccountGridType.php
+++ src/Grid/Shared/AccountGridType.php
@@ -12,54 +12,47 @@ use App\Common\Data\Operator\SortDescending;
 use App\Common\Form\Type\FilterType;
 use App\Common\Form\Type\PaginationType;
 use App\Common\Form\Type\SortType;
-use App\Entity\Master\MaterialCategory;
-use App\Entity\Master\MaterialSubCategory;
+use App\Entity\Master\AccountCategory;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MaterialGridType extends AbstractType
+class AccountGridType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('filter', FilterType::class, [
-                'field_names' => ['name', 'code', 'materialSubCategory', 'materialSubCategory:materialCategory'],
+                'field_names' => ['code', 'name', 'accountCategory'],
                 'field_label_list' => [
                     'name' => 'Nama',
                     'code' => 'Code',
-                    'materialSubCategory:materialCategory' => 'Category',
-                    'materialSubCategory' => 'Sub Category',
+                    'accountCategory' => 'Category',
                 ],
                 'field_operators_list' => [
-                    'name' => [FilterContain::class, FilterNotContain::class],
                     'code' => [FilterContain::class, FilterNotContain::class],
-                    'materialSubCategory:materialCategory' => [FilterEqual::class, FilterNotEqual::class],
-                    'materialSubCategory' => [FilterEqual::class, FilterNotEqual::class],
+                    'name' => [FilterContain::class, FilterNotContain::class],
+                    'accountCategory' => [FilterEqual::class, FilterNotEqual::class],
                 ],
                 'field_value_type_list' => [
-                    'materialSubCategory:materialCategory' => EntityType::class,
-                    'materialSubCategory' => EntityType::class,
+                    'accountCategory' => EntityType::class,
                 ],
                 'field_value_options_list' => [
-                    'materialSubCategory:materialCategory' => ['class' => MaterialCategory::class, 'choice_label' => 'name'],
-                    'materialSubCategory' => ['class' => MaterialSubCategory::class, 'choice_label' => 'name'],
+                    'accountCategory' => ['class' => AccountCategory::class, 'choice_label' => 'name'],
                 ],
             ])
             ->add('sort', SortType::class, [
-                'field_names' => ['name', 'code', 'materialCategory:name', 'materialSubCategory:name'],
+                'field_names' => ['code', 'name', 'accountCategory:name'],
                 'field_label_list' => [
                     'name' => 'Nama',
                     'code' => 'Code',
-                    'materialCategory:name' => 'Category',
-                    'materialSubCategory:name' => 'Sub Category',
+                    'accountCategory:name' => 'Category',
                 ],
                 'field_operators_list' => [
-                    'name' => [SortAscending::class, SortDescending::class],
                     'code' => [SortAscending::class, SortDescending::class],
-                    'materialCategory:name' => [SortAscending::class, SortDescending::class],
-                    'materialSubCategory:name' => [SortAscending::class, SortDescending::class],
+                    'name' => [SortAscending::class, SortDescending::class],
+                    'accountCategory:name' => [SortAscending::class, SortDescending::class],
                 ],
             ])
             ->add('pagination', PaginationType::class, ['size_choices' => [10, 20, 50, 100]])
