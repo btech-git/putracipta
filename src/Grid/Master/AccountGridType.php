65cb55e9155182f48b770b323f7a9d15c5796c17
pmahendro <pmahendro@bloomingtech-tp>
2023-03-08 09:38:25 +0700

    Modify master account grid type

--- src/Grid/Master/AccountGridType.php
+++ src/Grid/Master/AccountGridType.php
@@ -12,6 +12,8 @@ use App\Common\Data\Operator\SortDescending;
 use App\Common\Form\Type\FilterType;
 use App\Common\Form\Type\PaginationType;
 use App\Common\Form\Type\SortType;
+use App\Entity\Master\AccountCategory;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -23,25 +25,41 @@ class AccountGridType extends AbstractType
     {
         $builder
             ->add('filter', FilterType::class, [
-                'field_names' => ['code', 'name', 'isInactive'],
+                'field_names' => ['code', 'name', 'accountCategory', 'isInactive'],
+                'field_label_list' => [
+                    'name' => 'Nama',
+                    'code' => 'Code',
+                    'accountCategory' => 'Category',
+                    'isInactive' => 'Inactive',
+                ],
                 'field_operators_list' => [
                     'code' => [FilterContain::class, FilterNotContain::class],
                     'isInactive' => [FilterEqual::class, FilterNotEqual::class],
                     'name' => [FilterContain::class, FilterNotContain::class],
+                    'accountCategory' => [FilterEqual::class, FilterNotEqual::class],
                 ],
                 'field_value_type_list' => [
+                    'accountCategory' => EntityType::class,
                     'isInactive' => ChoiceType::class,
                 ],
                 'field_value_options_list' => [
+                    'accountCategory' => ['class' => AccountCategory::class, 'choice_label' => 'name'],
                     'isInactive' => ['choices' => ['Yes' => true, 'No' => false]],
                 ],
             ])
             ->add('sort', SortType::class, [
-                'field_names' => ['code', 'name', 'isInactive'],
+                'field_names' => ['code', 'name', 'accountCategory:name', 'isInactive'],
+                'field_label_list' => [
+                    'name' => 'Nama',
+                    'code' => 'Code',
+                    'accountCategory:name' => 'Category',
+                    'isInactive' => 'Inactive',
+                ],
                 'field_operators_list' => [
                     'code' => [SortAscending::class, SortDescending::class],
                     'isInactive' => [SortAscending::class, SortDescending::class],
                     'name' => [SortAscending::class, SortDescending::class],
+                    'accountCategory:name' => [SortAscending::class, SortDescending::class],
                 ],
             ])
             ->add('pagination', PaginationType::class, ['size_choices' => [10, 20, 50, 100]])
