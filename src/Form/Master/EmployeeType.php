6160bd2a3fc6b3aade0873b9d855f4547a08ec5d
pmahendro <pmahendro@bloomingtech-tp>
2023-03-09 12:22:06 +0700

    Modify master employee add relation user

--- src/Form/Master/EmployeeType.php
+++ src/Form/Master/EmployeeType.php
@@ -14,6 +14,7 @@ class EmployeeType extends AbstractType
     {
         $builder
             ->add('name')
+            ->add('user', null, ['choice_label' => 'username'])
             ->add('email')
             ->add('address')
             ->add('phone')
