50a918e2b901f661d28f55cb67b8666573974dbc
pmahendro <pmahendro@bloomingtech-tp>
2023-03-10 19:47:43 +0700

    Modify accounting grid type

--- src/Grid/Accounting/JournalVoucherHeaderGridType.php
+++ src/Grid/Accounting/JournalVoucherHeaderGridType.php
@@ -12,7 +12,10 @@ use App\Common\Data\Operator\SortDescending;
 use App\Common\Form\Type\FilterType;
 use App\Common\Form\Type\PaginationType;
 use App\Common\Form\Type\SortType;
+use App\Entity\AccountingHeader;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -22,29 +25,43 @@ class JournalVoucherHeaderGridType extends AbstractType
     {
         $builder
             ->add('filter', FilterType::class, [
-                'field_names' => ['isCanceled', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'createdTransactionDateTime', 'modifiedTransactionDateTime', 'approvedTransactionDateTime', 'transactionDate', 'note'],
+                'field_names' => ['isCanceled', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate', 'note'],
+                'field_label_list' => [
+                    'codeNumberOrdinal' => 'Code Number',
+                    'codeNumberMonth' => '',
+                    'codeNumberYear' => '',
+                    'transactionDate' => 'Tanggal',
+                ],
                 'field_operators_list' => [
                     'isCanceled' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberOrdinal' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberMonth' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberYear' => [FilterEqual::class, FilterNotEqual::class],
-                    'createdTransactionDateTime' => [FilterEqual::class, FilterNotEqual::class],
-                    'modifiedTransactionDateTime' => [FilterEqual::class, FilterNotEqual::class],
-                    'approvedTransactionDateTime' => [FilterEqual::class, FilterNotEqual::class],
                     'transactionDate' => [FilterEqual::class, FilterNotEqual::class],
                     'note' => [FilterContain::class, FilterNotContain::class],
                 ],
+                'field_value_type_list' => [
+                    'codeNumberOrdinal' => IntegerType::class,
+                    'codeNumberMonth' => ChoiceType::class,
+                    'codeNumberYear' => IntegerType::class,
+                ],
+                'field_value_options_list' => [
+                    'codeNumberMonth' => ['choices' => array_flip(AccountingHeader::MONTH_ROMAN_NUMERALS)],
+                    'transactionDate' => ['attr' => ['data-controller' => 'flatpickr-element']],
+                ],
             ])
             ->add('sort', SortType::class, [
-                'field_names' => ['isCanceled', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'createdTransactionDateTime', 'modifiedTransactionDateTime', 'approvedTransactionDateTime', 'transactionDate', 'note'],
+                'field_names' => ['isCanceled', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate', 'note', 'id'],
+                'field_label_list' => [
+                    'codeNumberOrdinal' => 'Code Number',
+                    'codeNumberMonth' => '',
+                    'codeNumberYear' => '',
+                    'transactionDate' => 'Tanggal',
+                    'customer:company' => 'Customer',
+                    'warehouse' => 'Gudang',
+                ],
                 'field_operators_list' => [
-                    'isCanceled' => [SortAscending::class, SortDescending::class],
-                    'codeNumberOrdinal' => [SortAscending::class, SortDescending::class],
-                    'codeNumberMonth' => [SortAscending::class, SortDescending::class],
-                    'codeNumberYear' => [SortAscending::class, SortDescending::class],
-                    'createdTransactionDateTime' => [SortAscending::class, SortDescending::class],
-                    'modifiedTransactionDateTime' => [SortAscending::class, SortDescending::class],
-                    'approvedTransactionDateTime' => [SortAscending::class, SortDescending::class],
+                    'id' => [SortAscending::class, SortDescending::class],
                     'transactionDate' => [SortAscending::class, SortDescending::class],
                     'note' => [SortAscending::class, SortDescending::class],
                 ],
