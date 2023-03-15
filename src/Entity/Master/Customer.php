f20377939b5453c388ac9c08d0b8750352ba1646
pmahendro <pmahendro@bloomingtech-tp>
2023-03-15 11:48:39 +0700

    Modify master entity

--- src/Entity/Master/Customer.php
+++ src/Entity/Master/Customer.php
@@ -85,6 +85,18 @@ class Customer extends Master
     #[ORM\Column(type: Types::TEXT)]
     private ?string $addressDelivery5 = '';
 
+    #[ORM\Column(length: 100)]
+    private ?string $name2 = '';
+
+    #[ORM\Column(length: 100)]
+    private ?string $name3 = '';
+
+    #[ORM\Column(length: 100)]
+    private ?string $name4 = '';
+
+    #[ORM\Column(length: 100)]
+    private ?string $name5 = '';
+
     public function __construct()
     {
         $this->products = new ArrayCollection();
@@ -325,6 +337,54 @@ class Customer extends Master
     public function setAddressDelivery5(string $addressDelivery5): self
     {
         $this->addressDelivery5 = $addressDelivery5;
+
+        return $this;
+    }
+
+    public function getName2(): ?string
+    {
+        return $this->name2;
+    }
+
+    public function setName2(string $name2): self
+    {
+        $this->name2 = $name2;
+
+        return $this;
+    }
+
+    public function getName3(): ?string
+    {
+        return $this->name3;
+    }
+
+    public function setName3(string $name3): self
+    {
+        $this->name3 = $name3;
+
+        return $this;
+    }
+
+    public function getName4(): ?string
+    {
+        return $this->name4;
+    }
+
+    public function setName4(string $name4): self
+    {
+        $this->name4 = $name4;
+
+        return $this;
+    }
+
+    public function getName5(): ?string
+    {
+        return $this->name5;
+    }
+
+    public function setName5(string $name5): self
+    {
+        $this->name5 = $name5;
 
         return $this;
     }
