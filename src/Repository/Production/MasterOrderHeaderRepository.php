7fc6cb4239ad55d10240b05215523111c364d961
pmahendro <pmahendro@bloomingtech-tp>
2023-06-01 20:37:21 +0700

    Add new production repo

--- src/Repository/Production/MasterOrderHeaderRepository.php
+++ src/Repository/Production/MasterOrderHeaderRepository.php
@@ -5,22 +5,22 @@ namespace App\Repository\Production;
 use App\Common\Doctrine\Repository\EntityAdd;
 use App\Common\Doctrine\Repository\EntityDataFetch;
 use App\Common\Doctrine\Repository\EntityRemove;
-use App\Entity\Production\MasterOrder;
+use App\Entity\Production\MasterOrderHeader;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-class MasterOrderRepository extends ServiceEntityRepository
+class MasterOrderHeaderRepository extends ServiceEntityRepository
 {
     use EntityDataFetch, EntityAdd, EntityRemove;
 
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, MasterOrder::class);
+        parent::__construct($registry, MasterOrderHeader::class);
     }
 
     public function findRecentBy($year, $month)
     {
-        $dql = 'SELECT e FROM ' . MasterOrder::class . ' e WHERE e.codeNumberMonth = :codeNumberMonth AND e.codeNumberYear = :codeNumberYear ORDER BY e.codeNumberOrdinal DESC';
+        $dql = 'SELECT e FROM ' . MasterOrderHeader::class . ' e WHERE e.codeNumberMonth = :codeNumberMonth AND e.codeNumberYear = :codeNumberYear ORDER BY e.codeNumberOrdinal DESC';
 
         $query = $this->getEntityManager()->createQuery($dql);
         $query->setParameter('codeNumberMonth', $month);
