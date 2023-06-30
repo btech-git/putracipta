983a26893f0e125faf29e1f0d75c795457d42435
darkstar <darkstar@lenovo>
2023-06-30 10:44:34 +0700

    Add sync scan command

--- src/Sync/Transaction/PurchaseOrderHeaderFormSync.php
+++ src/Sync/Transaction/PurchaseOrderHeaderFormSync.php
@@ -3,17 +3,32 @@
 namespace App\Sync\Transaction;
 
 use App\Common\Sync\EntitySyncScan;
-use Doctrine\ORM\EntityManagerInterface;
 
 class PurchaseOrderHeaderFormSync
 {
     use EntitySyncScan;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct()
     {
         $this->setupRelations([
             'purchaseOrderDetails' => [
-                'receiveDetails' => null,
+                'receiveDetails' => [
+                    'purchaseReturnDetails' => null,
+                    'purchaseInvoiceDetails' => null,
+                ],
+            ],
+            'receiveHeaders' => [
+                'receiveDetails' => [
+                    'purchaseReturnDetails' => null,
+                    'purchaseInvoiceDetails' => null,
+                ],
+                'purchaseInvoiceHeaders' => [
+                    'purchaseInvoiceDetails' => null,
+                    'purchasePaymentDetails' => null,
+                ],
+                'purchaseReturnHeaders' => [
+                    'purchaseReturnDetails' => null,
+                ],
             ],
         ]);
     }
