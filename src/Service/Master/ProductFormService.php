9d564de60a9580d07f30d31d717219b8a07672f6
pmahendro <pmahendro@bloomingtech-tp>
2023-07-12 20:22:18 +0700

    Modify product form service

--- src/Service/Master/ProductFormService.php
+++ src/Service/Master/ProductFormService.php
@@ -4,9 +4,11 @@ namespace App\Service\Master;
 
 use App\Entity\Master\DesignCode;
 use App\Entity\Master\DiecutKnife;
+use App\Entity\Master\DielineMillar;
 use App\Entity\Master\Product;
 use App\Repository\Master\DesignCodeRepository;
 use App\Repository\Master\DiecutKnifeRepository;
+use App\Repository\Master\DielineMillarRepository;
 use App\Repository\Master\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -16,6 +18,7 @@ class ProductFormService
     private ProductRepository $productRepository;
     private DesignCodeRepository $designCodeRepository;
     private DiecutKnifeRepository $diecutKnifeRepository;
+    private DielineMillarRepository $dielineMillarRepository;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
@@ -23,6 +26,7 @@ class ProductFormService
         $this->productRepository = $entityManager->getRepository(Product::class);
         $this->designCodeRepository = $entityManager->getRepository(DesignCode::class);
         $this->diecutKnifeRepository = $entityManager->getRepository(DiecutKnife::class);
+        $this->dielineMillarRepository = $entityManager->getRepository(DielineMillar::class);
     }
 
     public function finalize(Product $product, array $options = []): void
@@ -38,6 +42,10 @@ class ProductFormService
         foreach ($product->getDiecutKnives() as $diecutKnife) {
             $diecutKnife->setCustomer($product->getCustomer());
         }
+        
+        foreach ($product->getDielineMillars() as $dielineMillar) {
+            $dielineMillar->setCustomer($product->getCustomer());
+        }
     }
 
     public function save(Product $product, array $options = []): void
@@ -48,6 +56,9 @@ class ProductFormService
         }
         foreach ($product->getDiecutKnives() as $diecutKnife) {
             $this->diecutKnifeRepository->add($diecutKnife);
+        }
+        foreach ($product->getDielineMillars() as $dielineMillar) {
+            $this->dielineMillarRepository->add($dielineMillar);
         }
         $this->entityManager->flush();
     }
