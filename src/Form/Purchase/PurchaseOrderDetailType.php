2fd54b989f14dca1f45c0f9392b601f3df26c4df
pmahendro <pmahendro@bloomingtech>
2023-09-15 21:59:17 +0700

    Modify purchase form type

--- src/Form/Purchase/PurchaseOrderDetailType.php
+++ src/Form/Purchase/PurchaseOrderDetailType.php
@@ -8,7 +8,6 @@ use App\Entity\Master\Material;
 use App\Entity\Purchase\PurchaseOrderDetail;
 use App\Entity\Purchase\PurchaseRequestDetail;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
