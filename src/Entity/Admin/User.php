2750f889c87d4d20dc8ad835414e7d702e68ec41
pmahendro <pmahendro@bloomingtech>
2023-10-02 20:14:26 +0700

    Modify user entity

--- src/Entity/Admin/User.php
+++ src/Entity/Admin/User.php
@@ -2,9 +2,7 @@
 
 namespace App\Entity\Admin;
 
-use App\Entity\Master\Employee;
 use App\Repository\Admin\UserRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
