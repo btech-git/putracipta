eebd95e9684b24537b74e1234ae9d567c3f1a1c7
pmahendro <pmahendro@bloomingtech>
2023-10-07 18:11:15 +0700

    Modify accounting form type

--- src/Form/Accounting/JournalVoucherDetailType.php
+++ src/Form/Accounting/JournalVoucherDetailType.php
@@ -3,6 +3,7 @@
 namespace App\Form\Accounting;
 
 use App\Entity\Accounting\JournalVoucherDetail;
+use App\Common\Form\Type\FormattedNumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +13,8 @@ class JournalVoucherDetailType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('debitAmount')
-            ->add('creditAmount')
+            ->add('debitAmount', FormattedNumberType::class, ['decimals' => 2])
+            ->add('creditAmount', FormattedNumberType::class, ['decimals' => 2])
             ->add('isCanceled')
             ->add('memo')
             ->add('account', null, [
