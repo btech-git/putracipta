6da2d179c8c1d8926ac22f8e754bb52c4a8f64f7
pmahendro <pmahendro@bloomingtech>
2023-10-12 14:20:24 +0700

    Modify design code service

--- src/Service/Master/DesignCodeFormService.php
+++ src/Service/Master/DesignCodeFormService.php
@@ -37,4 +37,54 @@ class DesignCodeFormService
         }
         $this->entityManager->flush();
     }
+
+    public function copyFrom(DesignCode $sourceDesignCode): DesignCode
+    {
+        $designCode = new DesignCode();
+        $designCode->setName($sourceDesignCode->getName());
+        $designCode->setVariant($sourceDesignCode->getVariant());
+        $designCode->setCustomer($sourceDesignCode->getCustomer());
+        $designCode->setColor($sourceDesignCode->getColor());
+        $designCode->setPantone($sourceDesignCode->getPantone());
+        $designCode->setCoating($sourceDesignCode->getCoating());
+        $designCode->setCode($sourceDesignCode->getCode());
+        $designCode->setColorSpecial1($sourceDesignCode->getColorSpecial1());
+        $designCode->setColorSpecial2($sourceDesignCode->getColorSpecial2());
+        $designCode->setColorSpecial3($sourceDesignCode->getColorSpecial3());
+        $designCode->setColorSpecial4($sourceDesignCode->getColorSpecial4());
+        $designCode->setPrintingUpQuantity($sourceDesignCode->getPrintingUpQuantity());
+        $designCode->setPrintingKrisSize($sourceDesignCode->getPrintingKrisSize());
+        $designCode->setPaperMountage($sourceDesignCode->getPaperMountage());
+        $designCode->setDiecutKnife($sourceDesignCode->getDiecutKnife());
+        $designCode->setDielineMillar($sourceDesignCode->getDielineMillar());
+        $designCode->setStatus($sourceDesignCode->getStatus());
+        $designCode->setPaperCuttingLength($sourceDesignCode->getPaperCuttingLength());
+        $designCode->setPaperCuttingWidth($sourceDesignCode->getPaperCuttingWidth());
+        $designCode->setInkCyanPercentage($sourceDesignCode->getInkCyanPercentage());
+        $designCode->setInkMagentaPercentage($sourceDesignCode->getInkMagentaPercentage());
+        $designCode->setInkYellowPercentage($sourceDesignCode->getInkYellowPercentage());
+        $designCode->setInkBlackPercentage($sourceDesignCode->getInkBlackPercentage());
+        $designCode->setInkOpvPercentage($sourceDesignCode->getInkOpvPercentage());
+        $designCode->setInkK1Percentage($sourceDesignCode->getInkK1Percentage());
+        $designCode->setInkK2Percentage($sourceDesignCode->getInkK2Percentage());
+        $designCode->setInkK3Percentage($sourceDesignCode->getInkK3Percentage());
+        $designCode->setInkK4Percentage($sourceDesignCode->getInkK4Percentage());
+        $designCode->setPackagingGlueQuantity($sourceDesignCode->getPackagingGlueQuantity());
+        $designCode->setPackagingRubberQuantity($sourceDesignCode->getPackagingRubberQuantity());
+        $designCode->setPackagingPaperQuantity($sourceDesignCode->getPackagingPaperQuantity());
+        $designCode->setPackagingBoxQuantity($sourceDesignCode->getPackagingBoxQuantity());
+        $designCode->setPackagingTapeLargeQuantity($sourceDesignCode->getPackagingTapeLargeQuantity());
+        $designCode->setPackagingTapeSmallQuantity($sourceDesignCode->getPackagingTapeSmallQuantity());
+        $designCode->setPackagingPlasticQuantity($sourceDesignCode->getPackagingPlasticQuantity());
+        $designCode->setInsitPrintingPercentage($sourceDesignCode->getInsitPrintingPercentage());
+        $designCode->setInsitSortingPercentage($sourceDesignCode->getInsitSortingPercentage());
+        $designCode->setHotStamping($sourceDesignCode->getHotStamping());
+        foreach ($sourceDesignCode->getDesignCodeProcessDetails() as $sourceDesignCodeProcessDetail) {
+            $designCodeProcessDetail = new DesignCodeProcessDetail();
+            $designCodeProcessDetail->setDesignCode($sourceDesignCodeProcessDetail->getDesignCode());
+            $designCodeProcessDetail->setWorkOrderProcess($sourceDesignCodeProcessDetail->getWorkOrderProcess());
+            $designCode->addDesignCodeProcessDetail($designCodeProcessDetail);
+        }
+        return $designCode;
+    }
 }
