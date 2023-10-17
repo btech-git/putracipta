f2b758596a61caed8b116532a6e33d3064f2501a
pmahendro <pmahendro@bloomingtech>
2023-10-17 08:10:51 +0700

    Modify stock grid type

--- src/Grid/Stock/InventoryRequestHeaderGridType.php
+++ src/Grid/Stock/InventoryRequestHeaderGridType.php
@@ -12,7 +12,12 @@ use App\Common\Data\Operator\SortDescending;
 use App\Common\Form\Type\FilterType;
 use App\Common\Form\Type\PaginationType;
 use App\Common\Form\Type\SortType;
+use App\Entity\Master\Warehouse;
+use App\Entity\StockHeader;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -22,7 +27,14 @@ class InventoryRequestHeaderGridType extends AbstractType
     {
         $builder
             ->add('filter', FilterType::class, [
-                'field_names' => ['departmentName', 'workOrderNumber', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate'],
+                'field_names' => ['departmentName', 'workOrderNumber', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate', 'requestMode', 'warehouse'],
+                'field_label_list' => [
+                    'codeNumberOrdinal' => 'Code Number',
+                    'codeNumberMonth' => '',
+                    'codeNumberYear' => '',
+                    'transactionDate' => 'Tanggal',
+                    'warehouse' => 'Gudang',
+                ],
                 'field_operators_list' => [
                     'departmentName' => [FilterContain::class, FilterNotContain::class],
                     'workOrderNumber' => [FilterContain::class, FilterNotContain::class],
@@ -30,10 +42,30 @@ class InventoryRequestHeaderGridType extends AbstractType
                     'codeNumberMonth' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberYear' => [FilterEqual::class, FilterNotEqual::class],
                     'transactionDate' => [FilterEqual::class, FilterNotEqual::class],
+                    'requestMode' => [FilterEqual::class, FilterNotEqual::class],
+                    'warehouse' => [FilterEqual::class, FilterNotEqual::class],
+                ],
+                'field_value_type_list' => [
+                    'codeNumberOrdinal' => IntegerType::class,
+                    'codeNumberMonth' => ChoiceType::class,
+                    'codeNumberYear' => IntegerType::class,
+                    'warehouse' => EntityType::class,
+                ],
+                'field_value_options_list' => [
+                    'codeNumberMonth' => ['choices' => array_flip(StockHeader::MONTH_ROMAN_NUMERALS)],
+                    'transactionDate' => ['attr' => ['data-controller' => 'flatpickr-element']],
+                    'warehouse' => ['class' => Warehouse::class, 'choice_label' => 'name'],
                 ],
             ])
             ->add('sort', SortType::class, [
-                'field_names' => ['departmentName', 'workOrderNumber', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate'],
+                'field_names' => ['departmentName', 'workOrderNumber', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate', 'requestMode', 'warehouse'],
+                'field_label_list' => [
+                    'codeNumberOrdinal' => '',
+                    'codeNumberMonth' => '',
+                    'codeNumberYear' => 'Code Number',
+                    'transactionDate' => 'Tanggal',
+                    'warehouse' => 'Gudang',
+                ],
                 'field_operators_list' => [
                     'departmentName' => [SortAscending::class, SortDescending::class],
                     'workOrderNumber' => [SortAscending::class, SortDescending::class],
@@ -41,6 +73,8 @@ class InventoryRequestHeaderGridType extends AbstractType
                     'codeNumberMonth' => [SortAscending::class, SortDescending::class],
                     'codeNumberYear' => [SortAscending::class, SortDescending::class],
                     'transactionDate' => [SortAscending::class, SortDescending::class],
+                    'requestMode' => [SortAscending::class, SortDescending::class],
+                    'warehouse' => [SortAscending::class, SortDescending::class],
                 ],
             ])
             ->add('pagination', PaginationType::class, ['size_choices' => [10, 20, 50, 100]])
