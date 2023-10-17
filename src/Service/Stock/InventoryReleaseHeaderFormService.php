e6bfdb82c34f8a72f42ed8487a4a9d60521fb241
pmahendro <pmahendro@bloomingtech>
2023-10-17 08:14:11 +0700

    Modify stock service

--- src/Service/Stock/InventoryReleaseHeaderFormService.php
+++ src/Service/Stock/InventoryReleaseHeaderFormService.php
@@ -3,13 +3,20 @@
 namespace App\Service\Stock;
 
 use App\Common\Idempotent\IdempotentUtility;
+use App\Entity\Purchase\PurchaseOrderDetail;
+use App\Entity\Purchase\PurchaseOrderPaperDetail;
+use App\Entity\Stock\Inventory;
 use App\Entity\Stock\InventoryReleaseMaterialDetail;
 use App\Entity\Stock\InventoryReleasePaperDetail;
 use App\Entity\Stock\InventoryReleaseHeader;
 use App\Entity\Support\Idempotent;
+use App\Repository\Purchase\PurchaseOrderDetailRepository;
+use App\Repository\Purchase\PurchaseOrderPaperDetailRepository;
 use App\Repository\Stock\InventoryReleaseMaterialDetailRepository;
 use App\Repository\Stock\InventoryReleasePaperDetailRepository;
 use App\Repository\Stock\InventoryReleaseHeaderRepository;
+use App\Repository\Stock\InventoryRepository;
+use App\Util\Service\InventoryUtil;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -19,6 +26,9 @@ class InventoryReleaseHeaderFormService
     private InventoryReleaseHeaderRepository $inventoryReleaseHeaderRepository;
     private InventoryReleaseMaterialDetailRepository $inventoryReleaseMaterialDetailRepository;
     private InventoryReleasePaperDetailRepository $inventoryReleasePaperDetailRepository;
+    private PurchaseOrderDetailRepository $purchaseOrderDetailRepository;
+    private PurchaseOrderPaperDetailRepository $purchaseOrderPaperDetailRepository;
+    private InventoryRepository $inventoryRepository;
 
     public function __construct(RequestStack $requestStack, EntityManagerInterface $entityManager)
     {
@@ -28,6 +38,9 @@ class InventoryReleaseHeaderFormService
         $this->inventoryReleaseHeaderRepository = $entityManager->getRepository(InventoryReleaseHeader::class);
         $this->inventoryReleaseMaterialDetailRepository = $entityManager->getRepository(InventoryReleaseMaterialDetail::class);
         $this->inventoryReleasePaperDetailRepository = $entityManager->getRepository(InventoryReleasePaperDetail::class);
+        $this->purchaseOrderDetailRepository = $entityManager->getRepository(PurchaseOrderDetail::class);
+        $this->purchaseOrderPaperDetailRepository = $entityManager->getRepository(PurchaseOrderPaperDetail::class);
+        $this->inventoryRepository = $entityManager->getRepository(Inventory::class);
     }
 
     public function initialize(InventoryReleaseHeader $inventoryReleaseHeader, array $options = []): void
@@ -78,7 +91,7 @@ class InventoryReleaseHeaderFormService
                 $totalRelease += $inventoryReleaseMaterialDetail->getQuantity();
             }
             $inventoryRequestMaterialDetail->setQuantityRelease($totalRelease);
-            $inventoryRequestMaterialDetail->setQuantityRemaining($inventoryRequestMaterialDetail->getSyncRemainingDelivery());
+            $inventoryRequestMaterialDetail->setQuantityRemaining($inventoryRequestMaterialDetail->getSyncQuantityRemaining());
             
         }
         foreach ($inventoryReleaseHeader->getInventoryReleasePaperDetails() as $inventoryReleasePaperDetail) {
@@ -105,6 +118,39 @@ class InventoryReleaseHeaderFormService
         foreach ($inventoryReleaseHeader->getInventoryReleasePaperDetails() as $inventoryReleasePaperDetail) {
             $this->inventoryReleasePaperDetailRepository->add($inventoryReleasePaperDetail);
         }
+        $this->addInventories($inventoryReleaseHeader);
         $this->entityManager->flush();
+    }
+
+    private function addInventories(InventoryReleaseHeader $inventoryReleaseHeader): void
+    {
+        InventoryUtil::reverseOldData($this->inventoryRepository, $inventoryReleaseHeader);
+        if ($inventoryReleaseHeader->getReleaseMode() === InventoryReleaseHeader::RELEASE_MODE_MATERIAL) {
+            $inventoryReleaseMaterialDetails = $inventoryReleaseHeader->getInventoryReleaseMaterialDetails()->toArray();
+            $averagePriceList = InventoryUtil::getAveragePriceList('material', $this->purchaseOrderDetailRepository, $inventoryReleaseMaterialDetails);
+            InventoryUtil::addNewData($this->inventoryRepository, $inventoryReleaseHeader, $inventoryReleaseMaterialDetails, function($newInventory, $inventoryReleaseMaterialDetail) use ($averagePriceList, $inventoryReleaseHeader) {
+                $material = $inventoryReleaseMaterialDetail->getMaterial();
+                $purchasePrice = isset($averagePriceList[$material->getId()]) ? $averagePriceList[$material->getId()] : '0.00';
+                $newInventory->setTransactionSubject($inventoryReleaseMaterialDetail->getMemo());
+                $newInventory->setPurchasePrice($purchasePrice);
+                $newInventory->setMaterial($material);
+                $newInventory->setWarehouse($inventoryReleaseHeader->getWarehouse());
+                $newInventory->setInventoryMode($inventoryReleaseHeader->getReleaseMode());
+                $newInventory->setQuantityIn($inventoryReleaseMaterialDetail->getQuantity());
+            });
+        } else if ($inventoryReleaseHeader->getReleaseMode() === InventoryReleaseHeader::RELEASE_MODE_PAPER) {
+            $inventoryReleasePaperDetails = $inventoryReleaseHeader->getInventoryReleasePaperDetails()->toArray();
+            $averagePriceList = InventoryUtil::getAveragePriceList('paper', $this->purchaseOrderPaperDetailRepository, $inventoryReleasePaperDetails);
+            InventoryUtil::addNewData($this->inventoryRepository, $inventoryReleaseHeader, $inventoryReleasePaperDetails, function($newInventory, $inventoryReleasePaperDetail) use ($averagePriceList, $inventoryReleaseHeader) {
+                $paper = $inventoryReleasePaperDetail->getPaper();
+                $purchasePrice = isset($averagePriceList[$paper->getId()]) ? $averagePriceList[$paper->getId()] : '0.00';
+                $newInventory->setTransactionSubject($inventoryReleasePaperDetail->getMemo());
+                $newInventory->setPurchasePrice($purchasePrice);
+                $newInventory->setPaper($paper);
+                $newInventory->setWarehouse($inventoryReleaseHeader->getWarehouse());
+                $newInventory->setInventoryMode($inventoryReleaseHeader->getReleaseMode());
+                $newInventory->setQuantityIn($inventoryReleasePaperDetail->getQuantity());
+            });
+        }
     }
 }
