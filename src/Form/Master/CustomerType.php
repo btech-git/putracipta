0f9d4277aa4594be0084a0be08d777d3d222019b
pmahendro <pmahendro@bloomingtech>
2023-10-17 08:09:55 +0700

    Modify master form type

--- src/Form/Master/CustomerType.php
+++ src/Form/Master/CustomerType.php
@@ -14,6 +14,7 @@ class CustomerType extends AbstractType
         $builder
 //            ->add('code')
             ->add('company')
+            ->add('quantityTolerancePercentage', null, ['label' => 'Toleransi Quantity Order (%)'])
             ->add('name', null, ['label' => 'PIC 1'])
             ->add('name2', null, ['label' => 'PIC 2'])
             ->add('name3', null, ['label' => 'PIC 3'])
