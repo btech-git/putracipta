4cc2367f2c87be70eba5120d6dc425cc0ff27f2e
pmahendro <pmahendro@bloomingtech>
2023-10-31 20:00:52 +0700

    Modify transaction log repository

--- src/Repository/Support/TransactionLogRepository.php
+++ src/Repository/Support/TransactionLogRepository.php
@@ -5,16 +5,16 @@ namespace App\Repository\Support;
 use App\Common\Doctrine\Repository\EntityAdd;
 use App\Common\Doctrine\Repository\EntityDataFetch;
 use App\Common\Doctrine\Repository\EntityRemove;
-use App\Entity\Support\AccountingLog;
+use App\Entity\Support\TransactionLog;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-class AccountingLogRepository extends ServiceEntityRepository
+class TransactionLogRepository extends ServiceEntityRepository
 {
     use EntityDataFetch, EntityAdd, EntityRemove;
 
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, AccountingLog::class);
+        parent::__construct($registry, TransactionLog::class);
     }
 }
