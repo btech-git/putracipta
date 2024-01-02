44693812cc5ae6dae6939b19379bc6d43df0d543
pmahendro <pmahendro@bloomingtech>
2024-01-02 19:59:26 +0700

    Modify report controller

--- src/Controller/Report/SaleOrderHeaderController.php
+++ src/Controller/Report/SaleOrderHeaderController.php
@@ -6,10 +6,14 @@ use App\Common\Data\Criteria\DataCriteria;
 use App\Common\Data\Operator\FilterBetween;
 use App\Grid\Report\SaleOrderHeaderGridType;
 use App\Repository\Sale\SaleOrderHeaderRepository;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Reader\Html;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/report/sale_order_header')]
@@ -18,6 +22,48 @@ class SaleOrderHeaderController extends AbstractController
     #[Route('/_list', name: 'app_report_sale_order_header__list', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
     public function _list(Request $request, SaleOrderHeaderRepository $saleOrderHeaderRepository): Response
+    {
+        list($form, $count, $saleOrderHeaders) = $this->getReportData($request, $saleOrderHeaderRepository);
+
+        return $this->renderForm("report/sale_order_header/_list.html.twig", [
+            'form' => $form,
+            'count' => $count,
+            'saleOrderHeaders' => $saleOrderHeaders,
+        ]);
+    }
+
+    #[Route('/', name: 'app_report_sale_order_header_index', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function index(): Response
+    {
+        return $this->render("report/sale_order_header/index.html.twig");
+    }
+
+    #[Route('/export', name: 'app_report_sale_order_header_export', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function export(Request $request, SaleOrderHeaderRepository $saleOrderHeaderRepository): Response
+    {
+        list(, , $saleOrderHeaders) = $this->getReportData($request, $saleOrderHeaderRepository);
+        $htmlString = $this->renderView("report/sale_order_header/export.html.twig", [
+            'saleOrderHeaders' => $saleOrderHeaders,
+        ]);
+
+        $reader = new Html();
+        $spreadsheet = $reader->loadFromString($htmlString);
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $response =  new StreamedResponse(function() use ($writer) {
+            $writer->save('php://output');
+        });
+
+        $dispositionHeader = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'report.xlsx');
+        $response->headers->set('Content-Type', 'application/vnd.ms-excel');
+        $response->headers->set('Content-Disposition', $dispositionHeader);
+
+        return $response;
+    }
+
+    public function getReportData(Request $request, SaleOrderHeaderRepository $saleOrderHeaderRepository): array
     {
         $criteria = new DataCriteria();
         $currentDate = date('Y-m-d');
@@ -35,45 +81,6 @@ class SaleOrderHeaderController extends AbstractController
             }
         });
 
-        return $this->renderForm("report/sale_order_header/_list.html.twig", [
-            'form' => $form,
-            'count' => $count,
-            'saleOrderHeaders' => $saleOrderHeaders,
-        ]);
+        return [$form, $count, $saleOrderHeaders];
     }
-
-    #[Route('/', name: 'app_report_sale_order_header_index', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_USER')]
-    public function index(): Response
-    {
-        return $this->render("report/sale_order_header/index.html.twig");
-    }
-
-//    #[Route('/export', name: 'app_report_sale_order_header_export', methods: ['GET'])]
-//    #[IsGranted('ROLE_USER')]
-//    public function exportAction(Request $request)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//        $repository = $em->getRepository(SaleOrderHeader::class);
-//
-//        $grid = $this->get('lib.grid.datagrid');
-//        $grid->build(SaleOrderHeaderGridType::class, $repository, $request);
-//
-//        $excel = $this->get('phpexcel');
-//        $excelXmlReader = $this->get('lib.excel.xml_reader');
-//        $xml = $this->renderView('report/sale_order_header/export.xml.twig', array(
-//            'grid' => $grid->createView(),
-//        ));
-//        $excelObject = $excelXmlReader->load($xml);
-//        $writer = $excel->createWriter($excelObject, 'Excel5');
-//        $response = $excel->createStreamedResponse($writer);
-//
-//        $dispositionHeader = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'report.xls');
-//        $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
-//        $response->headers->set('Pragma', 'public');
-//        $response->headers->set('Cache-Control', 'maxage=1');
-//        $response->headers->set('Content-Disposition', $dispositionHeader);
-//
-//        return $response;
-//    }
 }
