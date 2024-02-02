12b7693f4f8b6d20f1daf2da2a65ae57512d9106
pmahendro <pmahendro@bloomingtech>
2024-02-02 13:39:20 +0700

    Modify report controller

--- src/Controller/Report/InventoryStockMaterialController.php
+++ src/Controller/Report/InventoryStockMaterialController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Report;
 
 use App\Common\Data\Criteria\DataCriteria;
 use App\Common\Data\Operator\FilterBetween;
+use App\Entity\Stock\Inventory;
 use App\Grid\Report\InventoryStockMaterialGridType;
+use App\Repository\Master\MaterialRepository;
 use App\Repository\Stock\InventoryRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,24 +19,33 @@ class InventoryStockMaterialController extends AbstractController
 {
     #[Route('/_list', name: 'app_report_inventory_stock_material__list', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_INVENTORY_REPORT')]
-    public function _list(Request $request, InventoryRepository $inventoryRepository): Response
+    public function _list(Request $request, MaterialRepository $materialRepository, InventoryRepository $inventoryRepository): Response
     {
         $criteria = new DataCriteria();
         $currentDate = date('Y-m-d');
         $criteria->setFilter([
-            'transactionDate' => [FilterBetween::class, $currentDate, $currentDate],
+            'inventory:transactionDate' => [FilterBetween::class, $currentDate, $currentDate],
         ]);
         $form = $this->createForm(InventoryStockMaterialGridType::class, $criteria);
         $form->handleRequest($request);
 
-        list($count, $inventoryStockMaterials) = $inventoryRepository->fetchData($criteria, function($qb, $alias) {
-            $qb->andWhere("{$alias}.product IS NOT null AND {$alias}.isReversed = 0");
+        list($count, $materials) = $materialRepository->fetchData($criteria, function($qb, $alias) use ($criteria) {
+            $qb->andWhere("{$alias}.isInactive = false");
+            $qb->andWhere("EXISTS (SELECT i.id FROM " . Inventory::class . " i WHERE {$alias} = i.material AND i.isReversed = false AND i.transactionDate BETWEEN :startDate AND :endDate)");
+            $qb->setParameter('startDate', $criteria->getFilter()['inventory:transactionDate'][1]);
+            $qb->setParameter('endDate', $criteria->getFilter()['inventory:transactionDate'][2]);
         });
+        $materialInventories = $inventoryRepository->findMaterialInventories($materials, $criteria->getFilter()['inventory:transactionDate'][1], $criteria->getFilter()['inventory:transactionDate'][2]);
+        $inventories = [];
+        foreach ($materialInventories as $materialInventory) {
+            $inventories[$materialInventory->getMaterial()->getId()][] = $materialInventory;
+        }
 
         return $this->renderForm("report/inventory_stock_material/_list.html.twig", [
             'form' => $form,
             'count' => $count,
-            'inventoryStockMaterials' => $inventoryStockMaterials,
+            'materials' => $materials,
+            'inventories' => $inventories,
         ]);
     }
 
--- src/Controller/Report/InventoryStockPaperController.php
+++ src/Controller/Report/InventoryStockPaperController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Report;
 
 use App\Common\Data\Criteria\DataCriteria;
 use App\Common\Data\Operator\FilterBetween;
-use App\Grid\Report\InventoryStockGridType;
+use App\Entity\Stock\Inventory;
+use App\Grid\Report\InventoryStockPaperGridType;
+use App\Repository\Master\PaperRepository;
 use App\Repository\Stock\InventoryRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,34 +14,45 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/report/inventory_stock')]
+#[Route('/report/inventory_stock_paper')]
 class InventoryStockPaperController extends AbstractController
 {
-    #[Route('/_list', name: 'app_report_inventory_stock__list', methods: ['GET', 'POST'])]
+    #[Route('/_list', name: 'app_report_inventory_stock_paper__list', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_INVENTORY_REPORT')]
-    public function _list(Request $request, InventoryRepository $inventoryRepository): Response
+    public function _list(Request $request, PaperRepository $paperRepository, InventoryRepository $inventoryRepository): Response
     {
         $criteria = new DataCriteria();
         $currentDate = date('Y-m-d');
         $criteria->setFilter([
-            'transactionDate' => [FilterBetween::class, $currentDate, $currentDate],
+            'inventory:transactionDate' => [FilterBetween::class, $currentDate, $currentDate],
         ]);
-        $form = $this->createForm(InventoryStockGridType::class, $criteria);
+        $form = $this->createForm(InventoryStockPaperGridType::class, $criteria);
         $form->handleRequest($request);
 
-        list($count, $inventoryStocks) = $inventoryRepository->fetchData($criteria);
+        list($count, $papers) = $paperRepository->fetchData($criteria, function($qb, $alias) use ($criteria) {
+            $qb->andWhere("{$alias}.isInactive = false");
+            $qb->andWhere("EXISTS (SELECT i.id FROM " . Inventory::class . " i WHERE {$alias} = i.paper AND i.isReversed = false AND i.transactionDate BETWEEN :startDate AND :endDate)");
+            $qb->setParameter('startDate', $criteria->getFilter()['inventory:transactionDate'][1]);
+            $qb->setParameter('endDate', $criteria->getFilter()['inventory:transactionDate'][2]);
+        });
+        $paperInventories = $inventoryRepository->findPaperInventories($papers, $criteria->getFilter()['inventory:transactionDate'][1], $criteria->getFilter()['inventory:transactionDate'][2]);
+        $inventories = [];
+        foreach ($paperInventories as $paperInventory) {
+            $inventories[$paperInventory->getPaper()->getId()][] = $paperInventory;
+        }
 
-        return $this->renderForm("report/inventory_stock/_list.html.twig", [
+        return $this->renderForm("report/inventory_stock_paper/_list.html.twig", [
             'form' => $form,
             'count' => $count,
-            'inventoryStocks' => $inventoryStocks,
+            'papers' => $papers,
+            'inventories' => $inventories,
         ]);
     }
 
-    #[Route('/', name: 'app_report_inventory_stock_index', methods: ['GET', 'POST'])]
+    #[Route('/', name: 'app_report_inventory_stock_paper_index', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_INVENTORY_REPORT')]
     public function index(): Response
     {
-        return $this->render("report/inventory_stock/index.html.twig");
+        return $this->render("report/inventory_stock_paper/index.html.twig");
     }
 }
