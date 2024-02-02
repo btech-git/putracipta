28a14137f7e363e0c245e0f0a2f1a86a4f9a0e93
pmahendro <pmahendro@bloomingtech>
2024-02-02 13:40:20 +0700

    Add stock report grid type

--- src/Grid/Report/InventoryStockPaperGridType.php
+++ src/Grid/Report/InventoryStockPaperGridType.php
@@ -21,7 +21,7 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class InventoryStockMaterialGridType extends AbstractType
+class InventoryStockPaperGridType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
