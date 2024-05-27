44fdf35510ff4ed84b8958548788fce678075620
pmahendro <pmahendro@bloomingtech>
2024-05-27 17:01:04 +0700

    Modify qc sorting controller

--- src/Controller/Production/QualityControlSortingHeaderController.php
+++ src/Controller/Production/QualityControlSortingHeaderController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Production;
 
 use App\Common\Data\Criteria\DataCriteria;
+use App\Common\Data\Operator\SortDescending;
+use App\Common\Idempotent\IdempotentUtility;
 use App\Entity\Production\QualityControlSortingHeader;
 use App\Form\Production\QualityControlSortingHeaderType;
 use App\Grid\Production\QualityControlSortingHeaderGridType;
 use App\Repository\Production\QualityControlSortingHeaderRepository;
+use App\Service\Production\QualityControlSortingHeaderFormService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,15 +19,25 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/production/quality_control_sorting_header')]
 class QualityControlSortingHeaderController extends AbstractController
 {
-    #[Route('/_list', name: 'app_production_quality_control_sorting_header__list', methods: ['GET'])]
-    #[IsGranted('ROLE_USER')]
+    #[Route('/_list', name: 'app_production_quality_control_sorting_header__list', methods: ['GET', 'POST'])]
+    #[Security("is_granted('ROLE_NEW_PRODUCT_ADD') or is_granted('ROLE_NEW_PRODUCT_EDIT')")]
     public function _list(Request $request, QualityControlSortingHeaderRepository $qualityControlSortingHeaderRepository): Response
     {
         $criteria = new DataCriteria();
-        $form = $this->createForm(QualityControlSortingHeaderGridType::class, $criteria, ['method' => 'GET']);
+        $criteria->setSort([
+            'transactionDate' => SortDescending::class,
+            'id' => SortDescending::class,
+        ]);
+        $form = $this->createForm(QualityControlSortingHeaderGridType::class, $criteria);
         $form->handleRequest($request);
 
-        list($count, $qualityControlSortingHeaders) = $qualityControlSortingHeaderRepository->fetchData($criteria);
+        list($count, $qualityControlSortingHeaders) = $qualityControlSortingHeaderRepository->fetchData($criteria, function($qb, $alias, $add) use ($request) {
+            if (isset($request->request->get('quality_control_sorting_header_grid')['filter']['customer:company']) && isset($request->request->get('quality_control_sorting_header_grid')['sort']['customer:company'])) {
+                $qb->innerJoin("{$alias}.customer", 's');
+                $add['filter']($qb, 's', 'company', $request->request->get('quality_control_sorting_header_grid')['filter']['customer:company']);
+                $add['sort']($qb, 's', 'company', $request->request->get('quality_control_sorting_header_grid')['sort']['customer:company']);
+            }
+        });
 
         return $this->renderForm("production/quality_control_sorting_header/_list.html.twig", [
             'form' => $form,
@@ -40,21 +53,23 @@ class QualityControlSortingHeaderController extends AbstractController
         return $this->render("production/quality_control_sorting_header/index.html.twig");
     }
 
-    #[Route('/new', name: 'app_production_quality_control_sorting_header_new', methods: ['GET', 'POST'])]
+    #[Route('/new.{_format}', name: 'app_production_quality_control_sorting_header_new', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
-    public function new(Request $request, QualityControlSortingHeaderRepository $qualityControlSortingHeaderRepository): Response
+    public function new(Request $request, QualityControlSortingHeaderFormService $qualityControlSortingHeaderFormService, $_format = 'html'): Response
     {
         $qualityControlSortingHeader = new QualityControlSortingHeader();
+        $qualityControlSortingHeaderFormService->initialize($qualityControlSortingHeader, ['datetime' => new \DateTime(), 'user' => $this->getUser()]);
         $form = $this->createForm(QualityControlSortingHeaderType::class, $qualityControlSortingHeader);
         $form->handleRequest($request);
+        $qualityControlSortingHeaderFormService->finalize($qualityControlSortingHeader);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $qualityControlSortingHeaderRepository->add($qualityControlSortingHeader, true);
+        if ($_format === 'html' && IdempotentUtility::check($request) && $form->isSubmitted() && $form->isValid()) {
+            $qualityControlSortingHeaderFormService->save($qualityControlSortingHeader);
 
             return $this->redirectToRoute('app_production_quality_control_sorting_header_show', ['id' => $qualityControlSortingHeader->getId()], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('production/quality_control_sorting_header/new.html.twig', [
+        return $this->renderForm("production/quality_control_sorting_header/new.{$_format}.twig", [
             'qualityControlSortingHeader' => $qualityControlSortingHeader,
             'form' => $form,
         ]);
@@ -69,20 +84,22 @@ class QualityControlSortingHeaderController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_production_quality_control_sorting_header_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit.{_format}', name: 'app_production_quality_control_sorting_header_edit', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
-    public function edit(Request $request, QualityControlSortingHeader $qualityControlSortingHeader, QualityControlSortingHeaderRepository $qualityControlSortingHeaderRepository): Response
+    public function edit(Request $request, QualityControlSortingHeader $qualityControlSortingHeader, QualityControlSortingHeaderFormService $qualityControlSortingHeaderFormService, $_format = 'html'): Response
     {
+        $qualityControlSortingHeaderFormService->initialize($qualityControlSortingHeader, ['datetime' => new \DateTime(), 'user' => $this->getUser()]);
         $form = $this->createForm(QualityControlSortingHeaderType::class, $qualityControlSortingHeader);
         $form->handleRequest($request);
+        $qualityControlSortingHeaderFormService->finalize($qualityControlSortingHeader);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $qualityControlSortingHeaderRepository->add($qualityControlSortingHeader, true);
+        if ($_format === 'html' && IdempotentUtility::check($request) && $form->isSubmitted() && $form->isValid()) {
+            $qualityControlSortingHeaderFormService->save($qualityControlSortingHeader);
 
             return $this->redirectToRoute('app_production_quality_control_sorting_header_show', ['id' => $qualityControlSortingHeader->getId()], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('production/quality_control_sorting_header/edit.html.twig', [
+        return $this->renderForm("production/quality_control_sorting_header/edit.{$_format}.twig", [
             'qualityControlSortingHeader' => $qualityControlSortingHeader,
             'form' => $form,
         ]);
