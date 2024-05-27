e45a48c80247ac006da25491236b640f87569958
pmahendro <pmahendro@bloomingtech>
2024-05-27 17:02:11 +0700

    Modify production grid type

--- src/Grid/Production/QualityControlSortingHeaderGridType.php
+++ src/Grid/Production/QualityControlSortingHeaderGridType.php
@@ -12,7 +12,10 @@ use App\Common\Data\Operator\SortDescending;
 use App\Common\Form\Type\FilterType;
 use App\Common\Form\Type\PaginationType;
 use App\Common\Form\Type\SortType;
+use App\Entity\ProductionHeader;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -22,33 +25,48 @@ class QualityControlSortingHeaderGridType extends AbstractType
     {
         $builder
             ->add('filter', FilterType::class, [
-                'field_names' => ['employeeInCharge', 'isCanceled', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'createdTransactionDateTime', 'modifiedTransactionDateTime', 'transactionDate', 'note', 'codeNumberVersion'],
+                'field_names' => ['codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'transactionDate', 'customer:company', 'employeeInCharge'],
+                'field_label_list' => [
+                    'codeNumberOrdinal' => 'Code Number',
+                    'codeNumberMonth' => '',
+                    'codeNumberYear' => '',
+                    'transactionDate' => 'Tanggal',
+                    'customer:company' => 'Customer',
+                    'employeeInCharge' => 'PIC',
+                ],
                 'field_operators_list' => [
                     'employeeInCharge' => [FilterContain::class, FilterNotContain::class],
-                    'isCanceled' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberOrdinal' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberMonth' => [FilterEqual::class, FilterNotEqual::class],
                     'codeNumberYear' => [FilterEqual::class, FilterNotEqual::class],
-                    'createdTransactionDateTime' => [FilterEqual::class, FilterNotEqual::class],
-                    'modifiedTransactionDateTime' => [FilterEqual::class, FilterNotEqual::class],
                     'transactionDate' => [FilterEqual::class, FilterNotEqual::class],
-                    'note' => [FilterContain::class, FilterNotContain::class],
-                    'codeNumberVersion' => [FilterEqual::class, FilterNotEqual::class],
+                    'customer:company' => [FilterContain::class, FilterNotContain::class],
+                ],
+                'field_value_type_list' => [
+                    'codeNumberOrdinal' => IntegerType::class,
+                    'codeNumberMonth' => ChoiceType::class,
+                    'codeNumberYear' => IntegerType::class,
+                ],
+                'field_value_options_list' => [
+                    'codeNumberMonth' => ['choices' => array_flip(ProductionHeader::MONTH_ROMAN_NUMERALS)],
+                    'transactionDate' => ['attr' => ['data-controller' => 'flatpickr-element']],
                 ],
             ])
             ->add('sort', SortType::class, [
-                'field_names' => ['employeeInCharge', 'isCanceled', 'codeNumberOrdinal', 'codeNumberMonth', 'codeNumberYear', 'createdTransactionDateTime', 'modifiedTransactionDateTime', 'transactionDate', 'note', 'codeNumberVersion'],
+                'field_names' => ['employeeInCharge', 'transactionDate', 'customer:company', 'id'],
+                'field_label_list' => [
+                    'codeNumberOrdinal' => 'Code Number',
+                    'codeNumberMonth' => '',
+                    'codeNumberYear' => '',
+                    'transactionDate' => 'Tanggal',
+                    'customer:company' => 'Customer',
+                    'employeeInCharge' => 'PIC',
+                ],
                 'field_operators_list' => [
+                    'id' => [SortAscending::class, SortDescending::class],
+                    'customer:company' => [SortAscending::class, SortDescending::class],
                     'employeeInCharge' => [SortAscending::class, SortDescending::class],
-                    'isCanceled' => [SortAscending::class, SortDescending::class],
-                    'codeNumberOrdinal' => [SortAscending::class, SortDescending::class],
-                    'codeNumberMonth' => [SortAscending::class, SortDescending::class],
-                    'codeNumberYear' => [SortAscending::class, SortDescending::class],
-                    'createdTransactionDateTime' => [SortAscending::class, SortDescending::class],
-                    'modifiedTransactionDateTime' => [SortAscending::class, SortDescending::class],
                     'transactionDate' => [SortAscending::class, SortDescending::class],
-                    'note' => [SortAscending::class, SortDescending::class],
-                    'codeNumberVersion' => [SortAscending::class, SortDescending::class],
                 ],
             ])
             ->add('pagination', PaginationType::class, ['size_choices' => [10, 20, 50, 100]])
