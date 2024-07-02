b4f41165bc761c059868d842fb3f4c5fbf4d78da
pmahendro <pmahendro@bloomingtech>
2024-07-02 19:32:08 +0700

    Modify sale order repository

--- src/Repository/Sale/SaleOrderHeaderRepository.php
+++ src/Repository/Sale/SaleOrderHeaderRepository.php
@@ -30,4 +30,20 @@ class SaleOrderHeaderRepository extends ServiceEntityRepository
 
         return $lastSaleOrderHeader;
     }
+    
+    public function findCustomerSaleOrderHeaders(array $customers, $startDate, $endDate): array
+    {
+        $dql = "SELECT e
+                FROM " . SaleOrderHeader::class . " e
+                WHERE e.customer IN (:customers) AND e.transactionDate BETWEEN :startDate AND :endDate
+                ORDER BY e.customer ASC, e.transactionDate ASC";
+
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('customers', $customers);
+        $query->setParameter('startDate', $startDate);
+        $query->setParameter('endDate', $endDate);
+        $saleOrderHeaders = $query->getResult();
+
+        return $saleOrderHeaders;
+    }
 }
