2b10de25c5654348e27f7ea7790dccff3b81f6f7
pmahendro <pmahendro@bloomingtech>
2024-07-04 07:30:07 +0700

    Modify production form type

--- src/Form/Production/MasterOrderHeaderType.php
+++ src/Form/Production/MasterOrderHeaderType.php
@@ -165,7 +165,7 @@ class MasterOrderHeaderType extends AbstractType
                             'application/pdf',
                         ],
                         'mimeTypesMessage' => 'Please upload a valid JPEG or PNG or PDF',
-                        'maxSizeMessage' => 'Please upload file size smaller than 5MB',
+                        'maxSizeMessage' => 'Please upload file size smaller than 10MB',
                     ])
                 ],
             ])
