cd68ec0e3284d9ffc575440195c93cef7cec02f6
pmahendro <pmahendro@bloomingtech>
2024-07-19 10:03:28 +0700

    Modify stock form type

--- src/Form/Stock/AdjustmentStockHeaderType.php
+++ src/Form/Stock/AdjustmentStockHeaderType.php
@@ -17,50 +17,56 @@ class AdjustmentStockHeaderType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $adjustmentModeChoices = $options['isFinishedGoods'] ? [
+            'Finished Goods' => AdjustmentStockHeader::ADJUSTMENT_MODE_PRODUCT,
+        ] : [
+            'Material' => AdjustmentStockHeader::ADJUSTMENT_MODE_MATERIAL,
+            'Kertas' => AdjustmentStockHeader::ADJUSTMENT_MODE_PAPER,
+        ];
         $builder
             ->add('transactionDate', FormattedDateType::class)
             ->add('note')
-            ->add('adjustmentMode', ChoiceType::class, ['multiple' => false, 'expanded' => false, 'choices' => [
-                'Material' => AdjustmentStockHeader::ADJUSTMENT_MODE_MATERIAL,
-                'Kertas' => AdjustmentStockHeader::ADJUSTMENT_MODE_PAPER,
-                'Finished Goods' => AdjustmentStockHeader::ADJUSTMENT_MODE_PRODUCT,
-            ]])
+            ->add('adjustmentMode', ChoiceType::class, ['multiple' => false, 'expanded' => false, 'placeholder' => '-- Select Mode --', 'choices' => $adjustmentModeChoices])
             ->add('warehouse', null, [
                 'choice_label' => 'name',
                 'query_builder' => function($repository) {
                     return $repository->createQueryBuilder('e')
                             ->andWhere("e.isInactive = false");
                 },
-            ])
-            ->add('adjustmentStockMaterialDetails', CollectionType::class, [
-                'entry_type' => AdjustmentStockMaterialDetailType::class,
-                'allow_add' => true,
-                'allow_delete' => true,
-                'by_reference' => false,
-                'prototype_data' => new AdjustmentStockMaterialDetail(),
-                'label' => false,
-            ])
-            ->add('adjustmentStockPaperDetails', CollectionType::class, [
-                'entry_type' => AdjustmentStockPaperDetailType::class,
-                'allow_add' => true,
-                'allow_delete' => true,
-                'by_reference' => false,
-                'prototype_data' => new AdjustmentStockPaperDetail(),
-                'label' => false,
-            ])
-            ->add('adjustmentStockProductDetails', CollectionType::class, [
+            ]);
+        if ($options['isFinishedGoods']) {
+            $builder->add('adjustmentStockProductDetails', CollectionType::class, [
                 'entry_type' => AdjustmentStockProductDetailType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
                 'by_reference' => false,
                 'prototype_data' => new AdjustmentStockProductDetail(),
                 'label' => false,
-            ])
-        ;
+            ]);
+        } else {
+            $builder
+                ->add('adjustmentStockMaterialDetails', CollectionType::class, [
+                    'entry_type' => AdjustmentStockMaterialDetailType::class,
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                    'by_reference' => false,
+                    'prototype_data' => new AdjustmentStockMaterialDetail(),
+                    'label' => false,
+                ])
+                ->add('adjustmentStockPaperDetails', CollectionType::class, [
+                    'entry_type' => AdjustmentStockPaperDetailType::class,
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                    'by_reference' => false,
+                    'prototype_data' => new AdjustmentStockPaperDetail(),
+                    'label' => false,
+                ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        $resolver->setRequired(['isFinishedGoods']);
         $resolver->setDefaults([
             'data_class' => AdjustmentStockHeader::class,
         ]);
