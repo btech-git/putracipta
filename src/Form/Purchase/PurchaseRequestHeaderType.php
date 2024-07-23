4c1a6485e3cda0ff50cc2ab44e64770ee72597ae
pmahendro <pmahendro@bloomingtech>
2024-07-23 21:43:53 +0700

    Modify purchase form type

--- src/Form/Purchase/PurchaseRequestHeaderType.php
+++ src/Form/Purchase/PurchaseRequestHeaderType.php
@@ -29,7 +29,8 @@ class PurchaseRequestHeaderType extends AbstractType
                 'choice_label' => 'name',
                 'query_builder' => function($repository) {
                     return $repository->createQueryBuilder('e')
-                            ->andWhere("e.isInactive = false");
+                            ->andWhere("e.isInactive = false")
+                            ->addOrderBy('e.name', 'ASC');
                 },
             ])
         ;
