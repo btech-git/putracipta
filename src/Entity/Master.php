9ce6b38606c877e432cd7df494fcf6d2ced920d6
pmahendro <pmahendro@bloomingtech>
2024-08-01 12:51:43 +0700

    Modify master entity

--- src/Entity/Master.php
+++ src/Entity/Master.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\MappedSuperclass]
+#[UniqueEntity(['name'])]
 abstract class Master
 {
     #[ORM\Column]
