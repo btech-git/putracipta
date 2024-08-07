c446e978f06112100fce1c2118d8ad7406019820
pmahendro <pmahendro@bloomingtech>
2024-08-07 21:58:15 +0700

    Modify master material form type

--- src/Form/Master/MaterialType.php
+++ src/Form/Master/MaterialType.php
@@ -3,6 +3,8 @@
 namespace App\Form\Master;
 
 use App\Entity\Master\Material;
+use App\Entity\Master\MaterialCategory;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +14,22 @@ class MaterialType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('materialCategory', EntityType::class, [
+                'mapped' => false,
+                'class' => MaterialCategory::class,
+                'choice_label' => 'name',
+                'query_builder' => function($repository) {
+                    return $repository->createQueryBuilder('e')
+                            ->andWhere("e.isInactive = false")
+                            ->andWhere("e.id <> 1")
+                            ->addOrderBy('e.name', 'ASC');
+                },
+            ])
             ->add('materialSubCategory', null, [
                 'choice_label' => 'name',
+                'choice_attr' => function($choice) {
+                    return ['data-material-category' => $choice->getMaterialCategory()->getId()];
+                },
                 'query_builder' => function($repository) {
                     return $repository->createQueryBuilder('e')
                             ->andWhere("e.isInactive = false")
