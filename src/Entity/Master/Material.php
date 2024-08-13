a8431f08a011b51691255ba36a143d88c6cb9275
pmahendro <pmahendro@bloomingtech>
2024-08-13 11:51:15 +0700

    Modify master entity

--- src/Entity/Master/Material.php
+++ src/Entity/Master/Material.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MaterialRepository::class)]
 #[ORM\Table(name: 'master_material')]
-//#[UniqueEntity(['code', 'name'])]
+#[UniqueEntity(['name'])]
 class Material extends Master
 {
     #[ORM\Id]
