4db6d49ee49853597aef119934898aff1ca2c54f
pmahendro <pmahendro@bloomingtech>
2024-08-19 23:02:34 +0700

    Add purchase report grid type

--- src/Grid/Report/ProductSaleOrderGridType.php
+++ src/Grid/Report/ProductSaleOrderGridType.php
@@ -14,7 +14,9 @@ use App\Common\Data\Operator\SortDescending;
 use App\Common\Form\Type\FilterType;
 use App\Common\Form\Type\PaginationType;
 use App\Common\Form\Type\SortType;
+use App\Entity\Master\Customer;
 use App\Entity\SaleHeader;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -27,7 +29,7 @@ class ProductSaleOrderGridType extends AbstractType
     {
         $builder
             ->add('filter', FilterType::class, [
-                'field_names' => ['saleOrderHeader:codeNumberOrdinal', 'saleOrderHeader:codeNumberMonth', 'saleOrderHeader:codeNumberYear', 'saleOrderHeader:transactionDate', 'saleOrderHeader:referenceNumber', 'name', 'saleOrderHeader:note', 'saleOrderHeader:transactionStatus'],
+                'field_names' => ['saleOrderHeader:codeNumberOrdinal', 'saleOrderHeader:codeNumberMonth', 'saleOrderHeader:codeNumberYear', 'saleOrderHeader:transactionDate', 'saleOrderHeader:referenceNumber', 'name', 'saleOrderHeader:note', 'saleOrderHeader:transactionStatus', 'customer'],
                 'field_label_list' => [
                     'saleOrderHeader:codeNumberOrdinal' => 'Code Number',
                     'saleOrderHeader:codeNumberMonth' => '',
@@ -44,19 +46,30 @@ class ProductSaleOrderGridType extends AbstractType
                     'saleOrderHeader:referenceNumber' => [FilterContain::class, FilterNotContain::class],
                     'saleOrderHeader:note' => [FilterContain::class, FilterNotContain::class],
                     'saleOrderHeader:transactionStatus' => [FilterEqual::class, FilterNotEqual::class],
+                    'customer' => [FilterEqual::class, FilterNotEqual::class],
                 ],
                 'field_value_type_list' => [
                     'saleOrderHeader:codeNumberOrdinal' => IntegerType::class,
                     'saleOrderHeader:codeNumberMonth' => ChoiceType::class,
                     'saleOrderHeader:codeNumberYear' => IntegerType::class,
+                    'customer' => EntityType::class,
                 ],
                 'field_value_options_list' => [
                     'saleOrderHeader:codeNumberMonth' => ['choices' => array_flip(SaleHeader::MONTH_ROMAN_NUMERALS)],
                     'saleOrderHeader:transactionDate' => ['attr' => ['data-controller' => 'flatpickr-element']],
+                    'customer' => [
+                        'class' => Customer::class, 
+                        'choice_label' => 'company',
+                        'query_builder' => function($repository) {
+                            return $repository->createQueryBuilder('e')
+                                    ->andWhere("e.isInactive = false")
+                                    ->addOrderBy('e.company', 'ASC');
+                        },
+                    ],
                 ],
             ])
             ->add('sort', SortType::class, [
-                'field_names' => ['saleOrderHeader:transactionDate', 'name', 'saleOrderHeader:referenceNumber', 'saleOrderHeader:note', 'saleOrderHeader:transactionStatus', 'saleOrderHeader:codeNumberYear', 'saleOrderHeader:codeNumberMonth', 'saleOrderHeader:codeNumberOrdinal'],
+                'field_names' => ['saleOrderHeader:transactionDate', 'customer', 'name', 'saleOrderHeader:referenceNumber', 'saleOrderHeader:note', 'saleOrderHeader:transactionStatus', 'saleOrderHeader:codeNumberYear', 'saleOrderHeader:codeNumberMonth', 'saleOrderHeader:codeNumberOrdinal'],
                 'field_label_list' => [
                     'saleOrderHeader:codeNumberOrdinal' => '',
                     'saleOrderHeader:codeNumberMonth' => '',
@@ -70,6 +83,7 @@ class ProductSaleOrderGridType extends AbstractType
                     'saleOrderHeader:codeNumberYear' => [SortAscending::class, SortDescending::class],
                     'saleOrderHeader:transactionDate' => [SortAscending::class, SortDescending::class],
                     'name' => [SortAscending::class, SortDescending::class],
+                    'customer' => [SortAscending::class, SortDescending::class],
                     'saleOrderHeader:referenceNumber' => [SortAscending::class, SortDescending::class],
                     'saleOrderHeader:note' => [SortAscending::class, SortDescending::class],
                     'saleOrderHeader:transactionStatus' => [SortAscending::class, SortDescending::class],
