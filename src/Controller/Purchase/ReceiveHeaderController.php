833eae23d7bbc5084814168108858dae90abe863
pmahendro <pmahendro@bloomingtech>
2024-08-24 18:33:47 +0700

    Modify purchase controller

--- src/Controller/Purchase/ReceiveHeaderController.php
+++ src/Controller/Purchase/ReceiveHeaderController.php
@@ -6,9 +6,10 @@ use App\Common\Data\Criteria\DataCriteria;
 use App\Common\Data\Operator\SortDescending;
 use App\Common\Form\Type\PaginationType;
 use App\Common\Idempotent\IdempotentUtility;
-use App\Entity\Purchase\ReceiveDetail;
 use App\Entity\Purchase\ReceiveHeader;
 use App\Form\Purchase\ReceiveHeaderType;
+use App\Grid\Purchase\PurchaseOrderDetailGridType;
+use App\Grid\Purchase\PurchaseOrderPaperDetailGridType;
 use App\Grid\Purchase\ReceiveHeaderGridType;
 use App\Repository\Purchase\PurchaseOrderDetailRepository;
 use App\Repository\Purchase\PurchaseOrderPaperDetailRepository;
@@ -41,11 +42,6 @@ class ReceiveHeaderController extends AbstractController
                 $add['filter']($qb, 's', 'company', $request->request->get('receive_header_grid')['filter']['supplier:company']);
                 $add['sort']($qb, 's', 'company', $request->request->get('receive_header_grid')['sort']['supplier:company']);
             }
-//            if (isset($request->request->get('receive_header_grid')['filter']['warehouse:name']) && isset($request->request->get('receive_header_grid')['sort']['warehouse:name'])) {
-//                $qb->innerJoin("{$alias}.warehouse", 'w');
-//                $add['filter']($qb, 'w', 'name', $request->request->get('receive_header_grid')['filter']['warehouse:name']);
-//                $add['sort']($qb, 'w', 'name', $request->request->get('receive_header_grid')['sort']['warehouse:name']);
-//            }
         });
 
         return $this->renderForm("purchase/receive_header/_list.html.twig", [
@@ -66,34 +62,93 @@ class ReceiveHeaderController extends AbstractController
     #[Security("is_granted('ROLE_RECEIVE_ADD') or is_granted('ROLE_RECEIVE_EDIT') or is_granted('ROLE_RECEIVE_VIEW') or is_granted('ROLE_APPROVAL')")]
     public function _listOutstandingPurchaseOrder(Request $request, PurchaseOrderDetailRepository $purchaseOrderDetailRepository, PurchaseOrderPaperDetailRepository $purchaseOrderPaperDetailRepository): Response
     {
-        $criteria = new DataCriteria();
-        $formMaterial = $this->createFormBuilder($criteria, ['data_class' => DataCriteria::class, 'csrf_protection' => false])
-                ->add('pagination', PaginationType::class, ['size_choices' => [10, 20, 50, 100]])
-                ->getForm();
+        $materialCriteria = new DataCriteria();
+        $formMaterial = $this->createForm(PurchaseOrderDetailGridType::class, $materialCriteria);
         $formMaterial->handleRequest($request);
 
-        list($countMaterial, $purchaseOrderDetails) = $purchaseOrderDetailRepository->fetchData($criteria, function($qb, $alias, $add, $new) {
+        list($countMaterial, $purchaseOrderDetails) = $purchaseOrderDetailRepository->fetchData($materialCriteria, function($qb, $alias, $add) use ($request) {
             $qb->andWhere("{$alias}.isCanceled = false");
             $qb->andWhere("{$alias}.remainingReceive > 0");
-//            $sub = $new(ReceiveDetail::class, 'r');
-//            $sub->andWhere("IDENTITY(r.purchaseOrderDetail) = {$alias}.id");
-//            $qb->andWhere($qb->expr()->not($qb->expr()->exists($sub->getDQL())));
             $qb->join("{$alias}.purchaseOrderHeader", 'h');
+            $qb->join("{$alias}.material", 'm');
             $qb->andWhere("h.transactionStatus IN ('approve', 'partial_receive')");
+            if (isset($request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:codeNumberOrdinal']) && isset($request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:codeNumberOrdinal'])) {
+                $add['filter']($qb, 'h', 'codeNumberOrdinal', $request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:codeNumberOrdinal']);
+                $add['sort']($qb, 'h', 'codeNumberOrdinal', $request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:codeNumberOrdinal']);
+            }
+            if (isset($request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:codeNumberMonth']) && isset($request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:codeNumberMonth'])) {
+                $add['filter']($qb, 'h', 'codeNumberMonth', $request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:codeNumberMonth']);
+                $add['sort']($qb, 'h', 'codeNumberMonth', $request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:codeNumberMonth']);
+            }
+            if (isset($request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:codeNumberYear']) && isset($request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:codeNumberYear'])) {
+                $add['filter']($qb, 'h', 'codeNumberYear', $request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:codeNumberYear']);
+                $add['sort']($qb, 'h', 'codeNumberYear', $request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:codeNumberYear']);
+            }
+            if (isset($request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:transactionDate']) && isset($request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:transactionDate'])) {
+                $add['filter']($qb, 'h', 'transactionDate', $request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:transactionDate']);
+                $add['sort']($qb, 'h', 'transactionDate', $request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:transactionDate']);
+            }
+            if (isset($request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:transactionStatus']) && isset($request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:transactionStatus'])) {
+                $add['filter']($qb, 'h', 'company', $request->request->get('purchase_order_detail_grid')['filter']['purchaseOrderHeader:transactionStatus']);
+                $add['sort']($qb, 'h', 'company', $request->request->get('purchase_order_detail_grid')['sort']['purchaseOrderHeader:transactionStatus']);
+            }
+            if (isset($request->request->get('purchase_order_detail_grid')['filter']['material:code']) && isset($request->request->get('purchase_order_detail_grid')['sort']['material:code'])) {
+                $add['filter']($qb, 'm', 'code', $request->request->get('purchase_order_detail_grid')['filter']['material:code']);
+                $add['sort']($qb, 'm', 'code', $request->request->get('purchase_order_detail_grid')['sort']['material:code']);
+            }
+            if (isset($request->request->get('purchase_order_detail_grid')['filter']['material:name']) && isset($request->request->get('purchase_order_detail_grid')['sort']['material:name'])) {
+                $add['filter']($qb, 'm', 'name', $request->request->get('purchase_order_detail_grid')['filter']['material:name']);
+                $add['sort']($qb, 'm', 'name', $request->request->get('purchase_order_detail_grid')['sort']['material:name']);
+            }
+            if (isset($request->request->get('purchase_order_detail_grid')['filter']['supplier:company']) && isset($request->request->get('purchase_order_detail_grid')['sort']['supplier:company'])) {
+                $qb->innerJoin("h.supplier", 's');
+                $add['filter']($qb, 's', 'company', $request->request->get('purchase_order_detail_grid')['filter']['supplier:company']);
+                $add['sort']($qb, 's', 'company', $request->request->get('purchase_order_detail_grid')['sort']['supplier:company']);
+            }
         });
 
-        $formPaper = $this->createFormBuilder($criteria, ['data_class' => DataCriteria::class, 'csrf_protection' => false])
-                ->add('pagination', PaginationType::class, ['size_choices' => [10, 20, 50, 100]])
-                ->getForm();
+        $paperCriteria = new DataCriteria();
+        $formPaper = $this->createForm(PurchaseOrderPaperDetailGridType::class, $paperCriteria);
         $formPaper->handleRequest($request);
-        list($countPaper, $purchaseOrderPaperDetails) = $purchaseOrderPaperDetailRepository->fetchData($criteria, function($qb, $alias, $add, $new) {
+        list($countPaper, $purchaseOrderPaperDetails) = $purchaseOrderPaperDetailRepository->fetchData($paperCriteria, function($qb, $alias, $add) use ($request) {
             $qb->andWhere("{$alias}.isCanceled = false");
             $qb->andWhere("{$alias}.remainingReceive > 0");
-//            $sub = $new(ReceiveDetail::class, 'r');
-//            $sub->andWhere("IDENTITY(r.purchaseOrderPaperDetail) = {$alias}.id");
-//            $qb->andWhere($qb->expr()->not($qb->expr()->exists($sub->getDQL())));
             $qb->join("{$alias}.purchaseOrderPaperHeader", 'h');
+            $qb->join("{$alias}.paper", 'p');
             $qb->andWhere("h.transactionStatus IN ('partial_receive')");
+            if (isset($request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:codeNumberOrdinal']) && isset($request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:codeNumberOrdinal'])) {
+                $add['filter']($qb, 'h', 'codeNumberOrdinal', $request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:codeNumberOrdinal']);
+                $add['sort']($qb, 'h', 'codeNumberOrdinal', $request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:codeNumberOrdinal']);
+            }
+            if (isset($request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:codeNumberMonth']) && isset($request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:codeNumberMonth'])) {
+                $add['filter']($qb, 'h', 'codeNumberMonth', $request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:codeNumberMonth']);
+                $add['sort']($qb, 'h', 'codeNumberMonth', $request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:codeNumberMonth']);
+            }
+            if (isset($request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:codeNumberYear']) && isset($request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:codeNumberYear'])) {
+                $add['filter']($qb, 'h', 'codeNumberYear', $request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:codeNumberYear']);
+                $add['sort']($qb, 'h', 'codeNumberYear', $request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:codeNumberYear']);
+            }
+            if (isset($request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:transactionDate']) && isset($request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:transactionDate'])) {
+                $add['filter']($qb, 'h', 'transactionDate', $request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:transactionDate']);
+                $add['sort']($qb, 'h', 'transactionDate', $request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:transactionDate']);
+            }
+            if (isset($request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:transactionStatus']) && isset($request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:transactionStatus'])) {
+                $add['filter']($qb, 'h', 'company', $request->request->get('purchase_order_paper_detail_grid')['filter']['purchaseOrderPaperHeader:transactionStatus']);
+                $add['sort']($qb, 'h', 'company', $request->request->get('purchase_order_paper_detail_grid')['sort']['purchaseOrderPaperHeader:transactionStatus']);
+            }
+            if (isset($request->request->get('purchase_order_paper_detail_grid')['filter']['paper:code']) && isset($request->request->get('purchase_order_paper_detail_grid')['sort']['paper:code'])) {
+                $add['filter']($qb, 'p', 'code', $request->request->get('purchase_order_paper_detail_grid')['filter']['paper:code']);
+                $add['sort']($qb, 'p', 'code', $request->request->get('purchase_order_paper_detail_grid')['sort']['paper:code']);
+            }
+            if (isset($request->request->get('purchase_order_paper_detail_grid')['filter']['paper:name']) && isset($request->request->get('purchase_order_paper_detail_grid')['sort']['paper:name'])) {
+                $add['filter']($qb, 'p', 'name', $request->request->get('purchase_order_paper_detail_grid')['filter']['paper:name']);
+                $add['sort']($qb, 'p', 'name', $request->request->get('purchase_order_paper_detail_grid')['sort']['paper:name']);
+            }
+            if (isset($request->request->get('purchase_order_paper_detail_grid')['filter']['supplier:company']) && isset($request->request->get('purchase_order_paper_detail_grid')['sort']['supplier:company'])) {
+                $qb->innerJoin("h.supplier", 's');
+                $add['filter']($qb, 's', 'company', $request->request->get('purchase_order_paper_detail_grid')['filter']['supplier:company']);
+                $add['sort']($qb, 's', 'company', $request->request->get('purchase_order_paper_detail_grid')['sort']['supplier:company']);
+            }
         });
 
         return $this->renderForm("purchase/receive_header/_list_outstanding_purchase_order.html.twig", [
@@ -106,7 +161,7 @@ class ReceiveHeaderController extends AbstractController
         ]);
     }
 
-    #[Route('/_list_outstanding_purchase_order', name: 'app_purchase_receive_header_index_outstanding_purchase_order', methods: ['GET'])]
+    #[Route('/index_outstanding_purchase_order', name: 'app_purchase_receive_header_index_outstanding_purchase_order', methods: ['GET'])]
     #[Security("is_granted('ROLE_RECEIVE_ADD') or is_granted('ROLE_RECEIVE_EDIT') or is_granted('ROLE_RECEIVE_VIEW') or is_granted('ROLE_APPROVAL')")]
     public function indexOutstandingPurchaseOrder(): Response
     {
