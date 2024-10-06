94cd322f658de36c4be2dff7946a57f73cc22226
pmahendro <pmahendro@bloomingtech>
2024-10-06 11:47:21 +0700

    Modify report controller

--- src/Controller/Report/SaleOrderDetailController.php
+++ src/Controller/Report/SaleOrderDetailController.php
@@ -4,8 +4,8 @@ namespace App\Controller\Report;
 
 use App\Common\Data\Criteria\DataCriteria;
 use App\Common\Data\Operator\FilterBetween;
-use App\Grid\Report\SaleOrderHeaderGridType;
-use App\Repository\Sale\SaleOrderHeaderRepository;
+use App\Grid\Report\SaleOrderDetailGridType;
+use App\Repository\Sale\SaleOrderDetailRepository;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Reader\Html;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -22,38 +22,39 @@ class SaleOrderDetailController extends AbstractController
 {
     #[Route('/_list', name: 'app_report_sale_order_detail__list', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_SALE_REPORT')]
-    public function _list(Request $request, SaleOrderHeaderRepository $saleOrderHeaderRepository): Response
+    public function _list(Request $request, SaleOrderDetailRepository $saleOrderDetailRepository): Response
     {
         $criteria = new DataCriteria();
         $currentDate = date('Y-m-d');
         $criteria->setFilter([
-            'orderReceiveDate' => [FilterBetween::class, $currentDate, $currentDate],
+            'saleOrderHeader:orderReceiveDate' => [FilterBetween::class, $currentDate, $currentDate],
         ]);
-        $form = $this->createForm(SaleOrderHeaderGridType::class, $criteria);
+        $form = $this->createForm(SaleOrderDetailGridType::class, $criteria);
         $form->handleRequest($request);
 
-        list($count, $saleOrderHeaders) = $saleOrderHeaderRepository->fetchData($criteria, function($qb, $alias, $add) use ($request) {
-            if (isset($request->request->get('sale_order_header_grid')['filter']['customer:company']) && isset($request->request->get('sale_order_header_grid')['sort']['customer:company'])) {
-                $qb->innerJoin("{$alias}.customer", 's');
-                $add['filter']($qb, 's', 'company', $request->request->get('sale_order_header_grid')['filter']['customer:company']);
-                $add['sort']($qb, 's', 'company', $request->request->get('sale_order_header_grid')['sort']['customer:company']);
+        list($count, $saleOrderDetails) = $saleOrderDetailRepository->fetchData($criteria, function($qb, $alias, $add) use ($request) {
+            $qb->innerJoin("{$alias}.saleOrderHeader", 'h');
+            if (isset($request->request->get('sale_order_detail_grid')['filter']['customer:company']) && isset($request->request->get('sale_order_detail_grid')['sort']['customer:company'])) {
+                $qb->innerJoin("h.customer", 'c');
+                $add['filter']($qb, 'c', 'company', $request->request->get('sale_order_detail_grid')['filter']['customer:company']);
+                $add['sort']($qb, 'c', 'company', $request->request->get('sale_order_detail_grid')['sort']['customer:company']);
             }
-            if (isset($request->request->get('sale_order_header_grid')['filter']['employee:name']) && isset($request->request->get('sale_order_header_grid')['sort']['employee:name'])) {
-                $qb->innerJoin("{$alias}.employee", 'm');
-                $add['filter']($qb, 'm', 'name', $request->request->get('sale_order_header_grid')['filter']['employee:name']);
-                $add['sort']($qb, 'm', 'name', $request->request->get('sale_order_header_grid')['sort']['employee:name']);
+            if (isset($request->request->get('sale_order_detail_grid')['filter']['employee:name']) && isset($request->request->get('sale_order_detail_grid')['sort']['employee:name'])) {
+                $qb->innerJoin("h.employee", 'm');
+                $add['filter']($qb, 'm', 'name', $request->request->get('sale_order_detail_grid')['filter']['employee:name']);
+                $add['sort']($qb, 'm', 'name', $request->request->get('sale_order_detail_grid')['sort']['employee:name']);
             }
-            $qb->addOrderBy("{$alias}.orderReceiveDate", 'ASC');
+            $qb->addOrderBy("h.orderReceiveDate", 'ASC');
             $qb->andWhere("{$alias}.isCanceled = false");
         });
 
         if ($request->request->has('export')) {
-            return $this->export($form, $saleOrderHeaders);
+            return $this->export($form, $saleOrderDetails);
         } else {
             return $this->renderForm("report/sale_order_detail/_list.html.twig", [
                 'form' => $form,
                 'count' => $count,
-                'saleOrderHeaders' => $saleOrderHeaders,
+                'saleOrderDetails' => $saleOrderDetails,
             ]);
         }
     }
@@ -65,11 +66,11 @@ class SaleOrderDetailController extends AbstractController
         return $this->render("report/sale_order_detail/index.html.twig");
     }
 
-    public function export(FormInterface $form, array $saleOrderHeaders): Response
+    public function export(FormInterface $form, array $saleOrderDetails): Response
     {
         $htmlString = $this->renderView("report/sale_order_detail/_list_export.html.twig", [
             'form' => $form->createView(),
-            'saleOrderHeaders' => $saleOrderHeaders,
+            'saleOrderDetails' => $saleOrderDetails,
         ]);
 
         $reader = new Html();
