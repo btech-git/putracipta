284569baf71e27caf6c4beb2d1f38d818d8b5524
pmahendro <pmahendro@bloomingtech>
2024-10-29 22:29:46 +0700

    Modify sale repository

--- src/Repository/Sale/SalePaymentDetailRepository.php
+++ src/Repository/Sale/SalePaymentDetailRepository.php
@@ -17,4 +17,34 @@ class SalePaymentDetailRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, SalePaymentDetail::class);
     }
+    
+    public function findSalePaymentHeaderDetails(array $salePaymentHeaders, $startDate, $endDate, $invoiceOrdinal, $invoiceMonth, $invoiceYear): array
+    {
+        $invoiceOrdinalConditionString = !empty($invoiceOrdinal) ? ' AND i.codeNumberOrdinal = :codeNumberOrdinal' : '';
+        $invoiceMonthConditionString = !empty($invoiceMonth) ? ' AND i.codeNumberMonth = :codeNumberMonth' : '';
+        $invoiceYearConditionString = !empty($invoiceYear) ? ' AND i.codeNumberYear = :codeNumberYear' : '';
+        $dql = "SELECT e
+                FROM " . SalePaymentDetail::class . " e
+                JOIN e.salePaymentHeader h
+                JOIN e.saleInvoiceHeader i
+                WHERE e.salePaymentHeader IN (:salePaymentHeaders) AND h.isCanceled = false AND h.transactionDate BETWEEN :startDate AND :endDate{$invoiceOrdinalConditionString}{$invoiceMonthConditionString}{$invoiceYearConditionString}
+                ORDER BY h.id ASC, h.transactionDate ASC";
+
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('salePaymentHeaders', $salePaymentHeaders);
+        $query->setParameter('startDate', $startDate);
+        $query->setParameter('endDate', $endDate);
+        if (!empty($invoiceOrdinal)) {
+            $query->setParameter('codeNumberOrdinal', $invoiceOrdinal);
+        }
+        if (!empty($invoiceMonth)) {
+            $query->setParameter('codeNumberMonth', $invoiceMonth);
+        }
+        if (!empty($invoiceYear)) {
+            $query->setParameter('codeNumberYear', $invoiceYear);
+        }
+        $salePaymentDetails = $query->getResult();
+
+        return $salePaymentDetails;
+    }
 }
