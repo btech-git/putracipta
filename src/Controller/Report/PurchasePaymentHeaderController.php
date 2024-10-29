c296638104ceac36293eb01094906dc7ebfebc12
pmahendro <pmahendro@bloomingtech>
2024-10-29 22:29:08 +0700

    Modify report controller

--- src/Controller/Report/PurchasePaymentHeaderController.php
+++ src/Controller/Report/PurchasePaymentHeaderController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Report;
 
 use App\Common\Data\Criteria\DataCriteria;
 use App\Common\Data\Operator\FilterBetween;
+use App\Entity\Purchase\PurchasePaymentDetail;
 use App\Grid\Report\PurchasePaymentHeaderGridType;
+use App\Repository\Purchase\PurchasePaymentDetailRepository;
 use App\Repository\Purchase\PurchasePaymentHeaderRepository;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Reader\Html;
@@ -22,7 +24,7 @@ class PurchasePaymentHeaderController extends AbstractController
 {
     #[Route('/_list', name: 'app_report_purchase_payment_header__list', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_FINANCE_REPORT')]
-    public function _list(Request $request, PurchasePaymentHeaderRepository $purchasePaymentHeaderRepository): Response
+    public function _list(Request $request, PurchasePaymentHeaderRepository $purchasePaymentHeaderRepository, PurchasePaymentDetailRepository $purchasePaymentDetailRepository): Response
     {
         $criteria = new DataCriteria();
         $currentDate = date('Y-m-d');
@@ -32,14 +34,23 @@ class PurchasePaymentHeaderController extends AbstractController
         $form = $this->createForm(PurchasePaymentHeaderGridType::class, $criteria);
         $form->handleRequest($request);
 
-        list($count, $purchasePaymentHeaders) = $purchasePaymentHeaderRepository->fetchData($criteria, function($qb, $alias, $add) use ($request) {
+        list($count, $purchasePaymentHeaders) = $purchasePaymentHeaderRepository->fetchData($criteria, function($qb, $alias, $add) use ($request, $criteria) {
             if (isset($request->request->get('purchase_payment_header_grid')['filter']['supplier:company']) && isset($request->request->get('purchase_payment_header_grid')['sort']['supplier:company'])) {
                 $qb->innerJoin("{$alias}.supplier", 's');
                 $add['filter']($qb, 's', 'company', $request->request->get('purchase_payment_header_grid')['filter']['supplier:company']);
                 $add['sort']($qb, 's', 'company', $request->request->get('purchase_payment_header_grid')['sort']['supplier:company']);
             }
             $qb->andWhere("{$alias}.isCanceled = false");
+            
+            $supplierInvoiceConditionString = !empty($criteria->getFilter()['purchaseInvoiceHeader:supplierInvoiceCodeNumber'][1]) ? ' AND i.supplierInvoiceCodeNumber LIKE :supplierInvoiceCodeNumber' : '';
+            $qb->andWhere("EXISTS (SELECT d.id FROM " . PurchasePaymentDetail::class . " d JOIN d.purchaseInvoiceHeader i WHERE {$alias} = d.purchasePaymentHeader AND d.isCanceled = false AND {$alias}.transactionDate BETWEEN :startDate AND :endDate{$supplierInvoiceConditionString})");
+            $qb->setParameter('startDate', $criteria->getFilter()['transactionDate'][1]);
+            $qb->setParameter('endDate', $criteria->getFilter()['transactionDate'][2]);
+            if (!empty($criteria->getFilter()['purchaseInvoiceHeader:supplierInvoiceCodeNumber'][1])) {
+                $qb->setParameter('supplierInvoiceCodeNumber', '%' . $criteria->getFilter()['purchaseInvoiceHeader:supplierInvoiceCodeNumber'][1] . '%');
+            }
         });
+        $purchasePaymentDetails = $this->getPurchasePaymentDetails($purchasePaymentDetailRepository, $criteria, $purchasePaymentHeaders);
 
         if ($request->request->has('export')) {
             return $this->export($form, $purchasePaymentHeaders);
@@ -48,6 +59,7 @@ class PurchasePaymentHeaderController extends AbstractController
                 'form' => $form,
                 'count' => $count,
                 'purchasePaymentHeaders' => $purchasePaymentHeaders,
+                'purchasePaymentDetails' => $purchasePaymentDetails, 
             ]);
         }
     }
@@ -57,6 +69,20 @@ class PurchasePaymentHeaderController extends AbstractController
     public function index(): Response
     {
         return $this->render("report/purchase_payment_header/index.html.twig");
+    }
+
+    private function getPurchasePaymentDetails(PurchasePaymentDetailRepository $purchasePaymentDetailRepository, DataCriteria $criteria, array $purchasePaymentHeaders): array
+    {
+        $startDate = $criteria->getFilter()['transactionDate'][1];
+        $endDate = $criteria->getFilter()['transactionDate'][2];
+        $supplierInvoiceCodeNumber = isset($criteria->getFilter()['purchaseInvoiceHeader:supplierInvoiceCodeNumber'][1]) ? $criteria->getFilter()['purchaseInvoiceHeader:supplierInvoiceCodeNumber'][1] : '';
+        $purchasePaymentHeaderDetails = $purchasePaymentDetailRepository->findPurchasePaymentHeaderDetails($purchasePaymentHeaders, $startDate, $endDate, $supplierInvoiceCodeNumber);
+        $purchasePaymentDetails = [];
+        foreach ($purchasePaymentHeaderDetails as $purchasePaymentHeaderDetail) {
+            $purchasePaymentDetails[$purchasePaymentHeaderDetail->getPurchasePaymentHeader()->getId()][] = $purchasePaymentHeaderDetail;
+        }
+
+        return $purchasePaymentDetails;
     }
 
     public function export(FormInterface $form, array $purchasePaymentHeaders): Response
--- src/Controller/Report/SalePaymentHeaderController.php
+++ src/Controller/Report/SalePaymentHeaderController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Report;
 
 use App\Common\Data\Criteria\DataCriteria;
 use App\Common\Data\Operator\FilterBetween;
+use App\Entity\Sale\SalePaymentDetail;
 use App\Grid\Report\SalePaymentHeaderGridType;
+use App\Repository\Sale\SalePaymentDetailRepository;
 use App\Repository\Sale\SalePaymentHeaderRepository;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Reader\Html;
@@ -22,7 +24,7 @@ class SalePaymentHeaderController extends AbstractController
 {
     #[Route('/_list', name: 'app_report_sale_payment_header__list', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_FINANCE_REPORT')]
-    public function _list(Request $request, SalePaymentHeaderRepository $salePaymentHeaderRepository): Response
+    public function _list(Request $request, SalePaymentHeaderRepository $salePaymentHeaderRepository, SalePaymentDetailRepository $salePaymentDetailRepository): Response
     {
         $criteria = new DataCriteria();
         $currentDate = date('Y-m-d');
@@ -32,7 +34,32 @@ class SalePaymentHeaderController extends AbstractController
         $form = $this->createForm(SalePaymentHeaderGridType::class, $criteria);
         $form->handleRequest($request);
 
-        list($count, $salePaymentHeaders) = $salePaymentHeaderRepository->fetchData($criteria);
+        list($count, $salePaymentHeaders) = $salePaymentHeaderRepository->fetchData($criteria, function($qb, $alias, $add) use ($request, $criteria) {
+            if (isset($request->request->get('sale_payment_header_grid')['filter']['customer:company']) && isset($request->request->get('sale_payment_header_grid')['sort']['customer:company'])) {
+                $qb->innerJoin("{$alias}.customer", 'c');
+                $add['filter']($qb, 'c', 'company', $request->request->get('sale_payment_header_grid')['filter']['customer:company']);
+                $add['sort']($qb, 'c', 'company', $request->request->get('sale_payment_header_grid')['sort']['customer:company']);
+            }
+            $qb->addOrderBy("{$alias}.transactionDate", 'ASC');
+            $qb->andWhere("{$alias}.isCanceled = false");
+            
+            $invoiceOrdinalConditionString = !empty($criteria->getFilter()['saleInvoiceHeader:codeNumberOrdinal'][1]) ? ' AND i.codeNumberOrdinal = :codeNumberOrdinal' : '';
+            $invoiceMonthConditionString = !empty($criteria->getFilter()['saleInvoiceHeader:codeNumberMonth'][1]) ? ' AND i.codeNumberMonth = :codeNumberMonth' : '';
+            $invoiceYearConditionString = !empty($criteria->getFilter()['saleInvoiceHeader:codeNumberYear'][1]) ? ' AND i.codeNumberYear = :codeNumberYear' : '';
+            $qb->andWhere("EXISTS (SELECT d.id FROM " . SalePaymentDetail::class . " d JOIN d.saleInvoiceHeader i WHERE {$alias} = d.salePaymentHeader AND d.isCanceled = false AND {$alias}.transactionDate BETWEEN :startDate AND :endDate{$invoiceOrdinalConditionString}{$invoiceMonthConditionString}{$invoiceYearConditionString})");
+            $qb->setParameter('startDate', $criteria->getFilter()['transactionDate'][1]);
+            $qb->setParameter('endDate', $criteria->getFilter()['transactionDate'][2]);
+            if (!empty($criteria->getFilter()['saleInvoiceHeader:codeNumberOrdinal'][1])) {
+                $qb->setParameter('codeNumberOrdinal', $criteria->getFilter()['saleInvoiceHeader:codeNumberOrdinal'][1]);
+            }
+            if (!empty($criteria->getFilter()['saleInvoiceHeader:codeNumberMonth'][1])) {
+                $qb->setParameter('codeNumberMonth', $criteria->getFilter()['saleInvoiceHeader:codeNumberMonth'][1]);
+            }
+            if (!empty($criteria->getFilter()['saleInvoiceHeader:codeNumberYear'][1])) {
+                $qb->setParameter('codeNumberYear', $criteria->getFilter()['saleInvoiceHeader:codeNumberYear'][1]);
+            }
+        });
+        $salePaymentDetails = $this->getSalePaymentDetails($salePaymentDetailRepository, $criteria, $salePaymentHeaders);
 
         if ($request->request->has('export')) {
             return $this->export($form, $salePaymentHeaders);
@@ -41,6 +68,7 @@ class SalePaymentHeaderController extends AbstractController
                 'form' => $form,
                 'count' => $count,
                 'salePaymentHeaders' => $salePaymentHeaders,
+                'salePaymentDetails' => $salePaymentDetails,
             ]);
         }
     }
@@ -50,6 +78,22 @@ class SalePaymentHeaderController extends AbstractController
     public function index(): Response
     {
         return $this->render("report/sale_payment_header/index.html.twig");
+    }
+
+    private function getSalePaymentDetails(SalePaymentDetailRepository $salePaymentDetailRepository, DataCriteria $criteria, array $salePaymentHeaders): array
+    {
+        $startDate = $criteria->getFilter()['transactionDate'][1];
+        $endDate = $criteria->getFilter()['transactionDate'][2];
+        $invoiceOrdinal = isset($criteria->getFilter()['saleInvoiceHeader:codeNumberOrdinal'][1]) ? $criteria->getFilter()['saleInvoiceHeader:codeNumberOrdinal'][1] : '';
+        $invoiceMonth = isset($criteria->getFilter()['saleInvoiceHeader:codeNumberMonth'][1]) ? $criteria->getFilter()['saleInvoiceHeader:codeNumberMonth'][1] : '';
+        $invoiceYear = isset($criteria->getFilter()['saleInvoiceHeader:codeNumberYear'][1]) ? $criteria->getFilter()['saleInvoiceHeader:codeNumberYear'][1] : '';
+        $salePaymentHeaderDetails = $salePaymentDetailRepository->findSalePaymentHeaderDetails($salePaymentHeaders, $startDate, $endDate, $invoiceOrdinal, $invoiceMonth, $invoiceYear);
+        $salePaymentDetails = [];
+        foreach ($salePaymentHeaderDetails as $salePaymentHeaderDetail) {
+            $salePaymentDetails[$salePaymentHeaderDetail->getSalePaymentHeader()->getId()][] = $salePaymentHeaderDetail;
+        }
+
+        return $salePaymentDetails;
     }
 
     public function export(FormInterface $form, array $salePaymentHeaders): Response
