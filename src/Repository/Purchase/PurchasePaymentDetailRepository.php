a886e3956be5c3df30de2818bb2606fd14d780fe
pmahendro <pmahendro@bloomingtech>
2024-10-29 22:29:39 +0700

    Modify purchase repository

--- src/Repository/Purchase/PurchasePaymentDetailRepository.php
+++ src/Repository/Purchase/PurchasePaymentDetailRepository.php
@@ -17,4 +17,26 @@ class PurchasePaymentDetailRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, PurchasePaymentDetail::class);
     }
+    
+    public function findPurchasePaymentHeaderDetails(array $purchasePaymentHeaders, $startDate, $endDate, $supplierInvoiceCodeNumber): array
+    {
+        $supplierInvoiceConditionString = !empty($supplierInvoiceCodeNumber) ? ' AND i.supplierInvoiceCodeNumber LIKE :supplierInvoiceCodeNumber' : '';
+        $dql = "SELECT e
+                FROM " . PurchasePaymentDetail::class . " e
+                JOIN e.purchasePaymentHeader h
+                JOIN e.purchaseInvoiceHeader i
+                WHERE e.purchasePaymentHeader IN (:purchasePaymentHeaders) AND h.isCanceled = false AND h.transactionDate BETWEEN :startDate AND :endDate{$supplierInvoiceConditionString}
+                ORDER BY h.id ASC, h.transactionDate ASC";
+
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('purchasePaymentHeaders', $purchasePaymentHeaders);
+        $query->setParameter('startDate', $startDate);
+        $query->setParameter('endDate', $endDate);
+        if (!empty($supplierInvoiceCodeNumber)) {
+            $query->setParameter('supplierInvoiceCodeNumber', '%' . $supplierInvoiceCodeNumber . '%');
+        }
+        $purchasePaymentDetails = $query->getResult();
+
+        return $purchasePaymentDetails;
+    }
 }
