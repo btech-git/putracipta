8f57388530997deb2c914e48f8c882d0ab25cd38
pmahendro <pmahendro@bloomingtech>
2024-11-27 08:00:32 +0700

    Modify sale repository

--- src/Repository/Sale/SaleReturnDetailRepository.php
+++ src/Repository/Sale/SaleReturnDetailRepository.php
@@ -17,4 +17,30 @@ class SaleReturnDetailRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, SaleReturnDetail::class);
     }
+    
+    public function findSaleReturnHeaderDetails(array $saleReturnHeaders, $startDate, $endDate, $productCode, $productName): array
+    {
+        $productCodeConditionString = !empty($productCode) ? ' AND p.code LIKE :productCode' : '';
+        $productNameConditionString = !empty($productName) ? ' AND p.name LIKE :productName' : '';
+        $dql = "SELECT e
+                FROM " . SaleReturnDetail::class . " e
+                JOIN e.saleReturnHeader h
+                JOIN e.product p
+                WHERE e.saleReturnHeader IN (:saleReturnHeaders) AND h.isCanceled = false AND h.transactionDate BETWEEN :startDate AND :endDate{$productCodeConditionString}{$productNameConditionString}
+                ORDER BY h.id ASC, h.transactionDate ASC";
+
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('saleReturnHeaders', $saleReturnHeaders);
+        $query->setParameter('startDate', $startDate);
+        $query->setParameter('endDate', $endDate);
+        if (!empty($productCode)) {
+            $query->setParameter('productCode', '%' . $productCode . '%');
+        }
+        if (!empty($productName)) {
+            $query->setParameter('productName', '%' . $productName . '%');
+        }
+        $saleReturnDetails = $query->getResult();
+
+        return $saleReturnDetails;
+    }
 }
